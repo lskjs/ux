a874c23f0fec339561523ee022eab7aaa95db683
Igor Suvorov <hi@isuvorov.ru>
2017-03-30 15:29:00 +0400

    additional tests

--- src/General/StatusButton/StatusButton.story.jsx
+++ src/General/StatusButton/StatusButton.story.jsx
@@ -2,34 +2,61 @@ import React from 'react'; //eslint-disable-line
 import StatusButton from './StatusButton';
 import TestPromise from './test/TestPromise';
 
-module.exports = ({ storiesOf }) => {
+module.exports = ({ storiesOf, action, knob }) => {
   return storiesOf('StatusButton', module)
     .addHtml(<link rel="stylesheet" type="text/css" href="https://yastatic.net/bootstrap/3.3.6/css/bootstrap.min.css" />)
-    .add('Default', () => (
-      <StatusButton>Status Button</StatusButton>
+    .add('--API', () => (
+      <StatusButton
+        onClick={action('onClick')}
+        bsStyle="info"
+        status={knob.text('status=null|loading|success|error', null)}
+        children={knob.text('children', 'Sample content')}
+      />
     ))
-    .add('Large', () => (
-      <StatusButton bsSize="large">Status Button</StatusButton>
+    .add('Default', () => (
+      <StatusButton onClick={action('onClick')}>Status Button</StatusButton>
     ))
     .add('Loading', () => (
-      <StatusButton status="loading">Click me!</StatusButton>
+      <StatusButton onClick={action('onClick')} status="loading">Click me!</StatusButton>
     ))
     .add('Success', () => (
-      <StatusButton status="success">Click me!</StatusButton>
+      <StatusButton onClick={action('onClick')} status="success">Click me!</StatusButton>
     ))
     .add('Error', () => (
-      <StatusButton status="error">Click me!</StatusButton>
+      <StatusButton onClick={action('onClick')} status="error">Click me!</StatusButton>
     ))
     .add('Promise success', () => (
-      <TestPromise />
+      <TestPromise
+        promiseTimeout={knob.number('promiseTimeout', 1000)}
+        timeout={knob.number('timeout', 2000)}
+      >
+        Click me!
+      </TestPromise>
     ))
     .add('Promise error', () => (
-      <TestPromise error />
+      <TestPromise
+        error
+        promiseTimeout={knob.number('promiseTimeout', 1000)}
+        timeout={knob.number('timeout', 2000)}
+      >
+        Click me!
+      </TestPromise>
     ))
-    .add('Large Promise success', () => (
-      <TestPromise bsSize="large" />
+    .add('Bootstrap styles', () => (
+      <StatusButton
+        onClick={action('onClick')}
+        bsStyle={knob.text('bsStyle', 'info')}
+        bsSize={knob.text('bsSize', 'large')}
+      >
+        {knob.text('children', 'Bootstrap styled button')}
+      </StatusButton>
     ))
-    .add('Large Promise error', () => (
-      <TestPromise bsSize="large" error />
-    ));
+    .add('Change tag=button', () => (
+      <StatusButton tag={knob.text('tag', 'button')} status="error">Click me!</StatusButton>
+    ))
+    .add('Random behaviour', () => {
+      return (<StatusButton bsSize="large" promise2={1}>
+        Рандомно ведущая себя кнопка
+      </StatusButton>);
+    });
 };
