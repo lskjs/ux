a38954c850eed58bd887218279eb726f1b6e5afb
Igor Suvorov <hi@isuvorov.ru>
2017-05-09 13:53:46 +0400

    new Avatar features

--- src/General/Avatar/Avatar.jsx
+++ src/General/Avatar/Avatar.jsx
@@ -1,118 +1,121 @@
 import React, { PropTypes } from 'react';
-// import PropTypes from 'prop-types';
-import importcss from 'importcss';
-// import classNames from 'classnames';
 import Component from 'lsk-general/General/Component';
+import _ from 'lodash';
+import ReactImageFallback from 'react-image-fallback';
+
+const defaultAvatar = '//cdn.mgbeta.ru/lsk/no-avatar.png';
+// import gifSpinner from './img/loading.gif';
+const textSizeRatio = 3;
 
 export default class Avatar extends Component {
 
   static defaultProps = {
-    src: '',
+    title: '',
+    name: '',
+    src: null,
+
     size: 64,
-    bgColor: '#9F9',
+    width: null,
+    height: null,
+
+    backgroundColor: '#838383',
+    textColor: '#d9d9d9',
+    textScale: 1,
+
     shape: 'circle',
-    color: '#FFF',
-    textSizeRatio: 3,
-    border: false,
-    borderColor: '#00BCD4',
-    borderWidth: 3,
-    shadow: false,
-    inactive: false,
-    badgeContent: '',
+
+    style: {},
+    innerStyle: {},
   }
 
   static propTypes = {
-    name: PropTypes.string.isRequired,
+    title: PropTypes.string,
     src: PropTypes.string,
+
     size: PropTypes.number,
-    bgColor: PropTypes.string,
-    shape: PropTypes.oneOfType(['circle', 'square', 'rounded']),
+    width: PropTypes.number,
+    height: PropTypes.number,
+
     color: PropTypes.string,
-    textSizeRatio: PropTypes.number,
-    border: PropTypes.bool,
-    borderColor: PropTypes.string,
-    borderWidth: PropTypes.number,
-    shadow: React.PropTypes.bool,
-    inactive: PropTypes.bool,
-    badgeContent: PropTypes.any,
+    textColor: PropTypes.string,
+    textScale: PropTypes.number,
+
+    shape: PropTypes.oneOfType(['circle', 'square', 'rounded']),
+
+    style: PropTypes.object,
+    innerStyle: PropTypes.object,
   };
 
-  renderAsImage() {
+  getInnerStyle() {
     const {
       size,
+      textScale,
       shape,
-      border,
-      borderColor,
-      borderWidth,
-      shadow,
-      inactive,
+      backgroundColor,
+      innerStyle,
+      textColor,
     } = this.props;
-    const name = this.props.title || this.props.name;
-    const src = this.props.src || this.props.avatar;
 
-    const imageStyle = {
-      width: size,
-      maxWidth: '100%',
-      height: size,
-      border: border ? `${borderWidth}px solid ${borderColor}` : 'none',
-      padding: border ? '2px' : 'none',
-      borderRadius: shape === 'circle' ? '50%' : shape === 'rounded' ? 6 : 0,
-      filter: inactive ? 'grayscale(100%)' : 'none',
-      boxShadow: shadow ? '1px 1px 10px 2px #ccc' : 'none',
+    const width = this.props.width || size;
+    const height = this.props.height || size;
+
+    const lineHeight = size;
+    const fontSize = Math.min(
+      Math.floor(size / textSizeRatio * textScale),
+      lineHeight,
+    );
+
+    let borderRadius = 'none';
+    if (shape === 'circle') {
+      borderRadius = '50%';
+    } else if (shape === 'rounded') {
+      borderRadius = 6;
+    }
+
+    return Object.assign({
       boxSizing: 'border-box',
-    };
+      maxWidth: '100%',
+      objectFit: 'cover',
+      overflow: 'hidden',
+      width,
+      height,
+      textAlign: 'center',
+      fontFamily: 'Helvetica, Arial, sans-serif',
+      borderRadius,
+      fontSize: `${fontSize}px`,
+      lineHeight: `${lineHeight}px`,
+      backgroundColor,
+      color: textColor,
+    }, innerStyle);
+  }
 
+  renderAsImage() {
+    const title = this.props.title || this.props.name;
+    const src = this.props.src || this.props.avatar;
     return (
-      <img
-        width={size}
-        height={size}
-        style={imageStyle}
+      <ReactImageFallback
         src={src}
-        alt={name}
-        title={name}
+        fallbackImage={defaultAvatar}
+        initialImage={defaultAvatar}
+        style={this.getInnerStyle()}
+        alt={title}
+        title={title}
       />
     );
   }
 
   renderAsText() {
-    const {
-      size,
-      bgColor,
-      shape,
-      color,
-      textSizeRatio,
-      border,
-      borderColor,
-      borderWidth,
-      shadow,
-      inactive,
-    } = this.props;
-    const name = this.props.title || this.props.name || '';
-    const src = this.props.src || this.props.avatar;
-
-    const initials = name
+    const title = this.props.title || this.props.name;
+    const initials = title
       .split(' ')
       .map(s => s.charAt(0))
       .join('');
-
-    const initialsStyle = {
-      width: size,
-      maxWidth: '100%',
-      height: size,
-      font: `${Math.floor(size / textSizeRatio)}px Helvetica, Arial, sans-serif`,
-      textAlign: 'center',
-      textTransform: 'uppercase',
-      lineHeight: `${size}px`,
-      color,
-      background: inactive ? 'grey' : bgColor,
-      border: border ? `${borderWidth}px solid ${borderColor}` : 'none',
-      padding: border ? '2px' : 'none',
-      borderRadius: shape === 'circle' ? '50%' : shape === 'rounded' ? 6 : 0,
-      boxShadow: shadow ? '2px 3px 8px 1px #ccc' : 'none',
-    };
-
     return (
-      <div style={initialsStyle} alt={name} title={name}>
+      <div
+        style={this.getInnerStyle()}
+        alt={title}
+        title={title}
+      >
         {initials}
       </div>
     );
@@ -120,27 +123,52 @@ export default class Avatar extends Component {
 
   render() {
     const {
-      src,
       size,
-      badgeContent,
+      style,
+      children,
     } = this.props;
 
-    const hostStyle = {
-      display: 'inline-block',
-      width: size,
-      height: size,
-      position: 'relative',
-    };
+    const src = this.props.src || this.props.avatar;
+    const width = this.props.width || size;
+    const height = this.props.height || size;
 
-    const Badge = badgeContent ? (<div>{ badgeContent }</div>) : null;
+    const wrapperStyle = Object.assign({
+      display: 'inline-block',
+      position: 'relative',
+      width,
+      height,
+    }, style);
+
 
     return (
-      <div style={{ display: 'inline-block' }}>
-        <div style={hostStyle}>
-          {src ? this.renderAsImage() : this.renderAsText()}
-          { Badge }
-        </div>
+      <div
+        style={wrapperStyle}
+      >
+        {src != null ? this.renderAsImage() : this.renderAsText()}
+        { children }
       </div>
     );
   }
 }
+
+Avatar.Badge = function (props) {
+  const offset = 6;
+  const style = {
+    position: 'absolute',
+  };
+
+  ['left', 'top', 'right', 'bottom'].forEach((dir) => {
+    if (!props[dir]) return;
+    if (typeof props[dir] === 'number') {
+      style[dir] = props[dir];
+    } else {
+      style[dir] = offset;
+    }
+  });
+
+  return (
+    <div style={style}>
+      {props.children}
+    </div>
+  );
+};
--- src/General/Avatar/Avatar.story.jsx
+++ src/General/Avatar/Avatar.story.jsx
@@ -2,53 +2,140 @@ import React from 'react';
 import Avatar from './Avatar';
 import _ from 'lodash';
 
-const rndNum = 1;
+const rndNum = _.random(99);
 const avatarImg = `https://randomuser.me/api/portraits/men/${rndNum}.jpg`;
+const smallImg = 'http://placeimg.com/32/32/people';
+const largeImg = 'http://placeimg.com/1920/1920/people';
+const noProportionalImg = 'http://placeimg.com/640/320/people';
 
-module.exports = function ({ storiesOf, action }) {
+const user = () => ({
+  title: 'John Smith',
+  src: `https://randomuser.me/api/portraits/men/${_.random(99)}.jpg`,
+});
+
+module.exports = function ({ storiesOf, action, knob }) {
   return storiesOf('Avatar', module)
-    .add('default', () => (
-      <Avatar src={avatarImg} name="John Smith" />
+    .add('Default as img', () => (
+      <Avatar
+        title="John Smith"
+        src={avatarImg}
+      />
     ))
-    .add('no avatar', () => (
-      <Avatar name="John Smith" />
+    .add('Alternative as User', () => (
+      <Avatar
+        name="John Smith"
+        avatar={avatarImg}
+      />
     ))
-    .add('sizes', () => (
+    .add('Empty', () => (
+      <Avatar />
+    ))
+    .add('No image', () => (
       <div>
-        <Avatar src={avatarImg} name="John Smith" size={32} />
-        <Avatar src={avatarImg} name="John Smith" size={64} />
-        <Avatar src={avatarImg} name="John Smith" size={100} />
+        <Avatar title="John Smith" />
+        <Avatar title="John Smith" textScale={0.5} />
+        <Avatar title="John Smith" textScale={2} />
+        <Avatar title="John Smith" textScale={4} />
       </div>
     ))
-    .add('border', () => (
+    .add('Size', () => (
       <div>
-        <Avatar src={avatarImg} name="John Smith" border />
-        <Avatar src={avatarImg} name="John Smith" border borderWidth={2} borderColor="#4CAF50" />
-        <Avatar src={avatarImg} name="John Smith" border borderWidth={4} borderColor="#F44336" />
+        <Avatar {...user()} />
+        <Avatar {...user()} size={32} />
+        <Avatar {...user()} size={64} />
+        <Avatar {...user()} size={100} />
+        <Avatar {...user()} width={100} height={50} />
+        <Avatar {...user()} width={50} height={100} />
       </div>
     ))
-    .add('badges', () => (
+    .add('Custom styles', () => (
       <div>
-        <Avatar src={avatarImg} name="John Smith" badgeContent={<div style={{ width: 10, height: 10, backgroundColor: '#4CAF50', borderRadius: '50%', position: 'absolute', right: '0px', bottom: '0px', border: '2px solid #fff' }} />} />
-        <Avatar name="Offline" badgeContent={<div style={{ width: 10, height: 10, backgroundColor: '#F44336', borderRadius: '50%', position: 'absolute', right: '0px', bottom: '0px', border: '2px solid #fff' }} />} />
+        <Avatar {...user()} style={{ margin: 10 }} />
+        <Avatar {...user()} style={{ padding: 10 }} />
+        <Avatar {...user()} style={{ border: '4px solid rgb(244, 67, 54)' }} />
+        <Avatar {...user()} innerStyle={{ boxShadow: '1px 1px 10px 2px #333' }} />
+        <Avatar {...user()} innerStyle={{ filter: 'grayscale(100%)' }} />
+        <Avatar {...user()} innerStyle={{ border: '4px solid rgb(244, 67, 54)' }} />
+        <Avatar {...user()} innerStyle={{ border: '2px solid #4CAF50'}} />
+        <Avatar {...user()} innerStyle={{ border: '4px solid #F44336' }} />
       </div>
     ))
-    .add('inactive', () => (
+    .add('Badge', () => (
       <div>
-        <Avatar src={avatarImg} name="John Smith" inactive />
-        <Avatar name="John Smith" inactive />
+        <Avatar {...user()}>
+          <Avatar.Badge left top>
+            <div style={{ width: 10, height: 10, backgroundColor: '#4CAF50', borderRadius: '50%', border: '2px solid #fff' }} />
+          </Avatar.Badge>
+        </Avatar>
+
+        <Avatar title="Offline Smith">
+          <Avatar.Badge right bottom>
+            <div style={{ width: 10, height: 10, backgroundColor: '#F44336', borderRadius: '50%', border: '2px solid #fff' }} />
+          </Avatar.Badge>
+        </Avatar>
+
+        <Avatar>
+          <Avatar.Badge left top >
+            <div style={{ width: 10, height: 10, backgroundColor: '#4CAF50', borderRadius: '50%', border: '2px solid #fff' }} />
+          </Avatar.Badge>
+          <Avatar.Badge right top >
+            <div style={{ width: 10, height: 10, backgroundColor: '#F44336', borderRadius: '50%', border: '2px solid #fff' }} />
+          </Avatar.Badge>
+          <Avatar.Badge left bottom >
+            <div style={{ width: 10, height: 10, backgroundColor: '#574caf', borderRadius: '50%', border: '2px solid #fff' }} />
+          </Avatar.Badge>
+          <Avatar.Badge right bottom >
+            <div style={{ width: 10, height: 10, backgroundColor: '#c6972f', borderRadius: '50%', border: '2px solid #fff' }} />
+          </Avatar.Badge>
+        </Avatar>
+
+        <Avatar>
+          <div style={{ width: 10, height: 10, backgroundColor: '#F44336', borderRadius: '50%', position: 'absolute', right: '24px', bottom: '24px', border: '2px solid #fff' }} />
+        </Avatar>
       </div>
     ))
-    .add('shadow', () => (
+    .add('Shape', () => (
       <div>
-        <Avatar src={avatarImg} name="John Smith" shadow />
+        <Avatar {...user()} shape="circle" />
+        <Avatar {...user()} shape="square" />
+        <Avatar {...user()} shape="rounded" />
       </div>
     ))
-    .add('shapes', () => (
+    .add('Failed img', () => (
+      <Avatar title="John Smith" src="/failed-img.png" />
+    ))
+    .add('No proportional', () => (
       <div>
-        <Avatar src={avatarImg} name="John Smith" shape="circle" />
-        <Avatar src={avatarImg} name="John Smith" shape="square" />
-        <Avatar src={avatarImg} name="John Smith" shape="rounded" />
+        <Avatar title="John Smith" src={smallImg} />
+        <Avatar title="John Smith" src={largeImg} />
+        <Avatar title="John Smith" src={noProportionalImg} />
+        <Avatar title="John Smith" src={noProportionalImg} width={128} height={64} />
+        <Avatar title="John Smith" src={noProportionalImg} shape="rounded" width={128} height={64} />
       </div>
+    ))
+    .add('With tag a', () => (
+      <a href="#"><Avatar {...user()} /></a>
+    ))
+    .add('Knobs', () => (
+      <Avatar
+        name={knob.string('Name')}
+        title={knob.string('Title')}
+        avatar={knob.string('Avatar')}
+        src={knob.string('Src')}
+        size={knob.number('Size', 64)}
+        width={knob.number('Width')}
+        height={knob.number('Height')}
+        shape={knob.select('Shape', { cirlce: 'circle', rounded: 'rounded', square: 'square' }, 'circle')}
+        border={knob.boolean('Border')}
+        borderColor={knob.color('Border color')}
+        borderWidth={knob.number('Border width')}
+        borderPadding={knob.number('Border padding')}
+        bgColor={knob.color('Background color')}
+        textColor={knob.color('Text color')}
+        textScale={knob.number('Text size scale')}
+        shadow={knob.boolean('Shadow')}
+        inactive={knob.boolean('Inactive')}
+        style={knob.object('Custom styles', { padding: '10px' })}
+      />
     ));
 };
