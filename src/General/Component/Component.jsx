579cfbb46d4ccbfccd4c26d62a407d6e8802d643
Igor Suvorov <hi@isuvorov.ru>
2017-11-14 11:49:33 +0200

    removeState

--- src/General/Component/Component.jsx
+++ src/General/Component/Component.jsx
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import cloneDeep from 'lodash/cloneDeep';
 import get from 'lodash/get';
 import set from 'lodash/set';
+import omit from 'lodash/omit';
 
 export default class Component extends React.Component {
   static contextTypes = {
@@ -19,6 +20,11 @@ export default class Component extends React.Component {
   setStatePath(path, value) {
     const state = cloneDeep(this.state);
     set(state, path, value);
+    return this.setStateAsync(state);
+  }
+  removeStatePath(path) {
+    const state = omit(this.state, path);
+    // console.log('removeStatePath', path, state);
     return this.setStateAsync(state);
   }
 
