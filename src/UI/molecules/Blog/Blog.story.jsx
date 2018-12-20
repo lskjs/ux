9644a9aeb849f18bc52a38203cca1165b1994d10
NightRomantic <nightromantic63@gmail.com>
2018-12-20 16:39:08 +0400

    Blog

---
+++ src/UI/molecules/Blog/Blog.story.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Blog from './Blog';
+import ThemeInjector from '../../../ThemeInjector';
+
+const articles = [
+  {
+    title: 'Dolor sit amet, consectetur adipisicing elit.',
+    avatar: 'assets/images-demo/avatars/1.jpg',
+    author: 'Jason Bourne',
+    category: 'Web Design',
+    description: 'Consequuntur hic eum ab consequatur, veniam laudantium placeat pariatur numquam quo voluptatem velit, labore voluptas tempore temporibus vitae tenetur porro eligendi exercitationem fugiat ipsum in ullam. Necessitatibus laboriosam enim ea eos, eveniet corporis impedit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+  },
+];
+
+module.exports = ({ storiesOf }) => (
+  storiesOf('Blog', module)
+    .add('Default', () => (
+      <div style={{ padding: 60 }}>
+        <ThemeInjector>
+          <Blog {...articles[0]} />
+        </ThemeInjector>
+      </div>
+    ))
+);
