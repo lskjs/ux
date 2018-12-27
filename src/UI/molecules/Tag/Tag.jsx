16d73bf6ae136c7e8b7e08841ae5ecdfa63f2bdb
Andrey Kondalov <i@yukio.ru>
2018-12-27 16:57:24 +0400

    Перенос TagsPicker, обновление FormGroup, перенос модалок и прочее

---
+++ src/UI/molecules/Tag/Tag.jsx
@@ -0,0 +1,25 @@
+import React, { PureComponent } from 'react';
+import Close from 'react-icons2/mdi/close';
+import { Title, CloseWrapper, TagItem } from './Tag.styles';
+
+class Tag extends PureComponent {
+  render() {
+    const { children, onClose, id, disabled } = this.props;
+    return (
+      <TagItem disabled={disabled}>
+        <Title>{children}</Title>
+        <If condition={onClose}>
+          <CloseWrapper
+            type="button"
+            onClick={() => onClose(id)}
+            disabled={disabled}
+          >
+            <Close />
+          </CloseWrapper>
+        </If>
+      </TagItem>
+    );
+  }
+}
+
+export default Tag;
