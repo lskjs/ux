6bc3731feea070235c62872d2ae6ac6da9b63838
Evgeny <nightromantic63@gmail.com>
2018-12-28 16:34:21 +0400

    EventFooter

--- src/UI/molecules/EventFooter/EventFooter.jsx
+++ src/UI/molecules/EventFooter/EventFooter.jsx
@@ -2,52 +2,53 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Avatar from '../../../Avatar';
 import {
-  UserFooterItem,
+  EventFooterItem,
   AvatarContainer,
   ButtonContainer,
   Content,
   TextContainer,
   TextItem,
-} from './UserBoxFooter.styles';
+} from './EventFooter.styles';
 
-class UserBoxFooter extends PureComponent {
+class EventFooter extends PureComponent {
   static propTypes = {
-    subtitle: PropTypes.string,
+    children: PropTypes.any,
     actions: PropTypes.any,
     user: PropTypes.object,
   };
   static defaultProps = {
-    subtitle: null,
+    children: null,
     actions: null,
     user: null,
   };
   render() {
     const {
-      subtitle,
       actions,
       user,
+      children,
     } = this.props;
     return (
-      <UserFooterItem>
-        <AvatarContainer>
-          <Avatar
-            title={user.title}
-            src={user.avatar}
-            id={user._id}
-            size={140}
-          />
-        </AvatarContainer>
+      <EventFooterItem>
         <Content>
+          <AvatarContainer>
+            <Avatar
+              title={user.title}
+              src={user.avatar}
+              id={user._id}
+              size={140}
+              shape="rounded"
+            />
+          </AvatarContainer>
           <TextContainer>
             <TextItem>{user.title}</TextItem>
-            <TextItem>{subtitle}</TextItem>
+            {children}
           </TextContainer>
           <ButtonContainer>
             {actions}
           </ButtonContainer>
         </Content>
-      </UserFooterItem>
+      </EventFooterItem>
     );
   }
 }
-export default UserBoxFooter;
+export default EventFooter;
---
+++ src/UI/molecules/EventFooter/EventFooter.story.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import EventFooter from './EventFooter';
+import ThemeInjector from '../../../ThemeInjector';
+
+module.exports = ({ storiesOf }) => (
+  storiesOf('ui/EventFooter', module)
+    .add('default', () => (
+      <ThemeInjector>
+        <div style={{ padding: 60, backgroundColor: 'grey' }}>
+          <EventFooter
+            user={{
+              title: 'Название эвента',
+              avatar: 'https://picsum.photos/200?random',
+              _id: 1,
+            }}
+          >
+            <div style={{ color: 'blue' }}>children element</div>
+          </EventFooter>
+        </div>
+      </ThemeInjector>
+    ))
+);
