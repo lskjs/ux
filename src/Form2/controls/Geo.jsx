ae275ec7e440d406bc749a05604fe0a882bbc5d0
Ната <natavts@gmail.com>
2019-01-03 21:01:00 +0200

    fix Geo

--- src/Form2/controls/Geo.jsx
+++ src/Form2/controls/Geo.jsx
@@ -1,34 +1,31 @@
 import React from 'react';
+import get from 'lodash/get';
 import MapBox from 'react-geo-picker/lib/mapbox';
 import GoogleMap from 'react-geo-picker/lib/google-map';
 
 const Geo = ({
   field,
   form,
-  // defaultValues,
   type,
-  value,
   ...props
 }) => {
-  // const defaultVal = defaultValues || [-122.268783, 37.871263];
+  const value = get(form.values, field.name);
   const coordinates = value ? {
     longitude: value?.[0],
     latitude: value?.[1],
-  } : {
-    longitude: 0,
-    latitude: 0,
-  };
+  } : null;
 
   const GeoComponent = type === 'google' ? GoogleMap : MapBox;
+  
   return (
     <div>
       <GeoComponent
         {...field}
         {...props}
-        // defaultValue={{
-        //   longitude: -111.268783,
-        //   latitude: 37.871263,
-        // }}
+        defaultValue={{
+          longitude: 0,
+          latitude: 0,
+        }}
         onChange={(val) => {
           form.setFieldValue(field.name, [val.longitude, val.latitude]);
         }}
