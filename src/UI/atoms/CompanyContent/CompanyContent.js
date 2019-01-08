ad4fdbcfe00793b8981818ca9e5e7bb8b5746997
Александр Можаев <nadymorigin@gmail.com>
2019-01-08 16:31:00 +0400

    Company atoms && fix LayoutHeader

---
+++ src/UI/atoms/CompanyContent/CompanyContent.js
@@ -0,0 +1,7 @@
+import styled from 'react-emotion';
+
+export default styled('div')`
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 10px;
+`;
---
+++ src/UI/atoms/CompanyContent/ContentSubtitle/ContentSubtitle.js
@@ -0,0 +1,6 @@
+import styled from 'react-emotion';
+
+export default styled('div')`
+  font-size: 25px;
+  padding: 5px 0;
+`;
