b3e28280c53f43c53275d8fd6b4b8469685461a7
Evgeny <nightromantic63@gmail.com>
2019-01-31 17:53:42 +0400

    horziontal fix, authPage fix, Button paints added

--- src/UI/atoms/Horizontal/Horizontal.js
+++ src/UI/atoms/Horizontal/Horizontal.js
@@ -6,7 +6,7 @@ const dynamicTag = createDynamicTag('div');
 const filteredTag = removeProps(dynamicTag, ['verticalAlign', 'align']);
 export default styled(filteredTag)`
   display: flex;
-  color: ${p => p.theme.colors.main};
+  color: inherit;
   text-decoration: none;
   align-items: ${p => (p.verticalAlign || 'flex-start')};
   justify-content: ${p => (p.align || 'flex-start')};
