efd1b21044032959a5ac62c6b60c543ab8b9e7e6
Igor Suvorov <hi@isuvorov.ru>
2019-02-01 03:45:21 +0200

    if tags null

--- src/Tags/Tags.jsx
+++ src/Tags/Tags.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Tags from '../UI/molecules/Tags';
 
-export default ({ options = [], values = [], ...props }) => (
-  <Tags
-    items={options.filter(option => values.includes(option.value))}
-    {...props}
-  />
+export default ({
+  options = [], nullValue, values = [], ...props
+}) => (
+  values.length === 0 && nullValue ? nullValue : (
+    <Tags
+      items={options.filter(option => values.includes(option.value))}
+      {...props}
+    />
+  )
 );
