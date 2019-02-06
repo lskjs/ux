11507d1e47a7b7cbd0e882df30f7d45971f5dfbe
Igor Suvorov <hi@isuvorov.ru>
2019-02-06 16:21:47 +0200

    Refactoring inside List

--- src/List/ListPaginator.jsx
+++ src/List/ListPaginator.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 import Pagination from 'antd/lib/pagination';
 import If from 'react-if';
-import T from '../T';
 import { contextToProps } from './List.context';
+import { PaginatorGroupWrapper } from './List.styles';
 
-@contextToProps('List', 'pageSize')
+@contextToProps('List', 'pageSize', 'show')
 @inject('listStore')
 @observer
 class ListPaginator extends Component {
@@ -13,31 +13,10 @@ class ListPaginator extends Component {
     const {
       List,
       listStore,
-      pageSize = 10,
     } = this.props;
-    const { options = [1, 2, 5, 10].map(a => a * pageSize) } = this.props;
-
-    const from = listStore.skip + 1;
-    const to = listStore.skip + listStore.items.length;
     const page = listStore.getCurrentPage();
     return (
       <List.PaginatorWrapper>
-        <List.PaginatorStepperWrapper>
-          <T name="lskList.paginatorShow" />
-          <List.PaginatorSelectWrapper
-            name="pagination-size"
-            value={listStore.limit}
-            onChange={e => listStore.setLimit(+e.target.value)}
-          >
-            {options.map(option => (<option key={option} value={option}>{option}</option>))}
-          </List.PaginatorSelectWrapper>
-        </List.PaginatorStepperWrapper>
-        <If condition={listStore.items.length}>
-          <List.PaginatorPagesWrapper>
-            {from}—{to}
-            {listStore.count !== null && ` / ${listStore.count}`}
-          </List.PaginatorPagesWrapper>
-        </If>
         <If condition={listStore.count === null}>
           <button disabled={!(page > 1)} onClick={() => listStore.setPage(page - 1)}>
             {'<'}
@@ -47,14 +26,14 @@ class ListPaginator extends Component {
           </button>
         </If>
         <If condition={listStore.count > 0}>
-          <List.PaginatorGroupWrapper>
+          <PaginatorGroupWrapper>
             <Pagination
               onChange={listStore.setPage}
               current={listStore.getCurrentPage()}
               pageSize={listStore.limit}
               total={listStore.count}
             />
-          </List.PaginatorGroupWrapper>
+          </PaginatorGroupWrapper>
         </If>
       </List.PaginatorWrapper>
     );
