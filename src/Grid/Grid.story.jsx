5be42d10f1b0caaa9ad2cc93eec234dacb41abb9
Igor Suvorov <hi@isuvorov.ru>
2019-02-07 16:28:23 +0200

    quick fix

--- src/Grid/Grid.story.jsx
+++ src/Grid/Grid.story.jsx
@@ -15,124 +15,63 @@ export default ({ storiesOf }) => {
     .add('Default', () => (
       <Story devtools>
         <Grid>
-          <Grid.Row>
-            <Grid.Col lg={6} md={12}>
+          <Row>
+            <Col lg={6} md={12}>
               <Item>
                 lg-6 md-12
               </Item>
-            </Grid.Col>
-            <Grid.Col lg={2} md={4} xs={6}>
+            </Col>
+            <Col lg={2} md={4} xs={6}>
               <Item>
                 lg-2 md-4 xs-6
               </Item>
-            </Grid.Col>
-            <Grid.Col lg={2} md={4} xs={6}>
+            </Col>
+            <Col lg={2} md={4} xs={6}>
               <Item>
                 lg-2 md-4 xs-6
               </Item>
-            </Grid.Col>
-            <Grid.Col lg={2} md={4} xs={12}>
+            </Col>
+            <Col lg={2} md={4} xs={12}>
               <Item>
                 lg-2 md-4 xs-12
               </Item>
-            </Grid.Col>
-          </Grid.Row>
+            </Col>
+          </Row>
         </Grid>
       </Story>
     ))
     .add('Vertical offsets', () => (
       <Story devtools>
         <Grid>
-          <Grid.Row vertical>
-            <Grid.Col lg={6} md={12}>
+          <Row vertical>
+            <Col lg={6} md={12}>
               <Item>
                 lg-6 md-12
               </Item>
-            </Grid.Col>
-            <Grid.Col lg={2} md={4} xs={6}>
+            </Col>
+            <Col lg={2} md={4} xs={6}>
               <Item>
                 lg-2 md-4 xs-6
               </Item>
-            </Grid.Col>
-            <Grid.Col lg={2} md={4} xs={6}>
+            </Col>
+            <Col lg={2} md={4} xs={6}>
               <Item>
                 lg-2 md-4 xs-6
               </Item>
-            </Grid.Col>
-            <Grid.Col lg={2} md={4} xs={12}>
+            </Col>
+            <Col lg={2} md={4} xs={12}>
               <Item>
                 lg-2 md-4 xs-12
               </Item>
-            </Grid.Col>
-          </Grid.Row>
+            </Col>
+          </Row>
         </Grid>
       </Story>
     ))
     .add('Custom gap', () => (
       <Story devtools>
         <Grid>
-          <Grid.Row vertical gap={32}>
-            <Grid.Col md={4}>
-              <Item>
-                Grid.Col 4
-              </Item>
-            </Grid.Col>
-            <Grid.Col md={4}>
-              <Item>
-                Grid.Col 4
-              </Item>
-            </Grid.Col>
-            <Grid.Col md={4}>
-              <Item>
-                Grid.Col 4
-              </Item>
-            </Grid.Col>
-          </Grid.Row>
-        </Grid>
-      </Story>
-    ))
-    .add('Vertical offsets with custom gap', () => (
-      <Story devtools>
-        <Grid>
-          <Grid.Row vertical gap={6}>
-            <Grid.Col md={4}>
-              <Item>
-                Grid.Col 4
-              </Item>
-            </Grid.Col>
-            <Grid.Col md={4}>
-              <Item>
-                Grid.Col 4
-              </Item>
-            </Grid.Col>
-            <Grid.Col md={4}>
-              <Item>
-                Grid.Col 4
-              </Item>
-            </Grid.Col>
-            <Grid.Col md={4}>
-              <Item>
-                Grid.Col 4
-              </Item>
-            </Grid.Col>
-            <Grid.Col md={4}>
-              <Item>
-                Grid.Col 4
-              </Item>
-            </Grid.Col>
-            <Grid.Col md={4}>
-              <Item>
-                Grid.Col 4
-              </Item>
-            </Grid.Col>
-          </Grid.Row>
-        </Grid>
-      </Story>
-    ))
-    .add('Destructing imports', () => (
-      <Story devtools>
-        <Grid>
-          <Row>
+          <Row vertical gap={32}>
             <Col md={4}>
               <Item>
                 Col 4
@@ -150,6 +89,65 @@ export default ({ storiesOf }) => {
             </Col>
           </Row>
         </Grid>
+      </Story>
+    ))
+    .add('Vertical offsets with custom gap', () => (
+      <Story devtools>
+        <Grid>
+          <Row vertical gap={6}>
+            <Col md={4}>
+              <Item>
+                Col 4
+              </Item>
+            </Col>
+            <Col md={4}>
+              <Item>
+                Col 4
+              </Item>
+            </Col>
+            <Col md={4}>
+              <Item>
+                Col 4
+              </Item>
+            </Col>
+            <Col md={4}>
+              <Item>
+                Col 4
+              </Item>
+            </Col>
+            <Col md={4}>
+              <Item>
+                Col 4
+              </Item>
+            </Col>
+            <Col md={4}>
+              <Item>
+                Col 4
+              </Item>
+            </Col>
+          </Row>
+        </Grid>
+      </Story>
+    ))
+    .add('without container', () => (
+      <Story devtools>
+        <Row>
+          <Col md={4}>
+            <Item>
+              Col 4
+            </Item>
+          </Col>
+          <Col md={4}>
+            <Item>
+              Col 4
+            </Item>
+          </Col>
+          <Col md={4}>
+            <Item>
+              Col 4
+            </Item>
+          </Col>
+        </Row>
       </Story>
     ));
 };
