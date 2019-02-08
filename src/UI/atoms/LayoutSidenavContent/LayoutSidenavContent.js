4b5dd1a5da843efeeae58273f8473f521c5e8646
Evgeny <nightromantic63@gmail.com>
2019-02-08 14:20:37 +0400

    fix GP-243

--- src/UI/atoms/LayoutSidenavContent/LayoutSidenavContent.js
+++ src/UI/atoms/LayoutSidenavContent/LayoutSidenavContent.js
@@ -5,6 +5,7 @@ export default styled('div')`
   z-index: 1;
   flex: 1 1 auto;
   overflow: auto;
+  overflow-x: hidden;
   border-right: 1px solid rgba(0,0,0,.1);
   font-family: ${p => p.theme.fontFamily};
 `;
