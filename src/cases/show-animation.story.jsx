75859c32118604e0dcf43b3ab312c84477ef80c1
Igor Suvorov <hi@isuvorov.ru>
2019-02-19 17:01:13 +0200

    fixes

--- src/cases/show-animation.story.jsx
+++ src/cases/show-animation.story.jsx
@@ -4,22 +4,26 @@ import range from 'lodash/range';
 import Story from '../Story';
 import { Container, Row, Col } from '../Grid';
 import BlogCard from '../UI/molecules/BlogCard';
+import Incrementator from './Incrementator';
+
 
 export default ({ storiesOf }) => (
   storiesOf('_cases', module)
-    .add('show-animation', () => (
+    .add('show-animation with', () => (
       <Story>
         <Container>
-          <Row vertical>
-            <QueueAnim
-              type="bottom"
-              delay={3000}
-              duration={1100}
-            >
-              {range(10).map(i => (
-                <Col key={i} md={3}>
-                  <BlogCard
-                    {...{
+          {/* <Row vertical> */}
+          <QueueAnim
+            type="bottom"
+            delay={100}
+            duration={300}
+            component={Row}
+            componentProps={{ vertical: true }}
+          >
+            {range(30).map(i => (
+              <Col key={i} md={3}>
+                <BlogCard
+                  {...{
                     img: 'http://iarouse.com/dist-react-ant-design/v1.1.4/assets/images-demo/assets/600_400-1.jpg',
                     date: 'May 1st',
                     title: `Be Yourself ${i}`,
@@ -27,15 +31,69 @@ export default ({ storiesOf }) => (
                     tag: 'Quotes',
                     author: 'Oscar Wilde',
                   }}
-                  />
-                </Col>
+                />
+              </Col>
             ))}
-            </QueueAnim>
+          </QueueAnim>
+          {/* </Row> */}
+        </Container>
+      </Story>
+    ))
+    .add('show-animation without', () => (
+      <Story>
+        <Container>
+          <Row vertical>
+            {range(30).map(i => (
+              <Col key={i} md={3}>
+                <BlogCard
+                  {...{
+                    img: 'http://iarouse.com/dist-react-ant-design/v1.1.4/assets/images-demo/assets/600_400-1.jpg',
+                    date: 'May 1st',
+                    title: `Be Yourself ${i}`,
+                    content: 'Be yourself; everyone else is already taken.',
+                    tag: 'Quotes',
+                    author: 'Oscar Wilde',
+                  }}
+                />
+              </Col>
+            ))}
           </Row>
         </Container>
       </Story>
     ))
-    .add('show-animation2', () => (
+    .add('show-animation-2', () => (
+      <Story>
+        <Incrementator count={10}>
+          {count => (
+            <Container>
+              <QueueAnim
+                type="bottom"
+                delay={100}
+                duration={300}
+                component={Row}
+                componentProps={{ vertical: true }}
+              >
+                {range(count).map(i => (
+                  <Col key={i} md={3}>
+                    <BlogCard
+                      {...{
+                        img: 'http://iarouse.com/dist-react-ant-design/v1.1.4/assets/images-demo/assets/600_400-1.jpg',
+                        date: 'May 1st',
+                        title: `Be Yourself ${i}`,
+                        content: 'Be yourself; everyone else is already taken.',
+                        tag: 'Quotes',
+                        author: 'Oscar Wilde',
+                      }}
+                    />
+                  </Col>
+                ))}
+              </QueueAnim>
+            </Container>
+          )}
+        </Incrementator>
+      </Story>
+    ))
+    .add('show-animation-3', () => (
       <Story>
         <Container>
           <Row vertical>
