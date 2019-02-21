96f0f1e51b0522b3a18e0a61c98c25356730370f
Igor Suvorov <hi@isuvorov.ru>
2019-02-21 13:43:45 +0200

    createFormWithI18 with user

--- src/Form2/createFormWithI18.js
+++ src/Form2/createFormWithI18.js
@@ -8,19 +8,21 @@ const createFormWithI18 = (callback) => {
     return createForm(callback);
   }
 
-  @inject('i18', 'config')
+  @inject('i18', 'config', 'user')
   // @observer
   class Component2 extends Component {
     shouldComponentUpdate(nextProps) {
       return !!(nextProps.hash && this.props.hash && nextProps.hash !== this.props.hash);
     }
     render() {
-      const { i18, config, ...props } = this.props;
-      const params = callback({ i18, config });
+      const {
+        i18, config, user, ...props
+      } = this.props;
+      const params = callback({ i18, config, user });
       const Component3 = createForm(params);
       // console.log('createFormWithI18', props);
 
-      return <Component3 {...props} />;
+      return <Component3 i18={i18} config={config} user={user} {...props} />;
     }
   }
 
