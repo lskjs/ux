797cec1ec8e41daa329dc338ffa078255b744d01
Андрей Чуриков <shitric2@gmail.com>
2019-03-01 20:18:27 +0400

    clear

--- src/Form2/controls/CheckBlock/CheckBlock.jsx
+++ src/Form2/controls/CheckBlock/CheckBlock.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import get from 'lodash/get';
+// import get from 'lodash/get';
 import ExtendedCheckblock from '../../../UI/molecules/ExtendedCheckblock';
 
 const CheckBlock = ({
