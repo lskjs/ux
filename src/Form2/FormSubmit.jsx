6e9e59db1181914a539ea366cb6fe43961909a28
Igor Suvorov <hi@isuvorov.ru>
2019-03-06 17:19:21 +0200

    FormSubmit

---
+++ src/Form2/FormSubmit.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Form from 'antd/lib/form';
+import Button from '../Button';
+import getControlHtmlId from './getControlHtmlId';
+import FormError from './FormError';
+
+export default ({
+  componentClass: Component = Button, errors, onSubmit, status, ...props
+}) => (
+  <Form.Item
+    validateStatus={errors ? 'error' : null}
+  >
+    {errors.onSubmit && <FormError id={getControlHtmlId('onSubmit')}>{errors.onSubmit}</FormError>}
+    <Component
+      paint="primary"
+      type="submit"
+      state={status}
+      disabled={!!status}
+      {...props}
+    />
+  </Form.Item>
+);
