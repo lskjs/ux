750c822ea33ef0950556dcc25057150d7a3d1a3d
Evgeny <nightromantic63@gmail.com>
2019-03-06 18:01:06 +0400

    AntTag

---
+++ src/UI/molecules/AntTag/AntTag.styles.js
@@ -0,0 +1,17 @@
+import styled, { css } from 'react-emotion';
+import { Tag } from 'antd/lib';
+import removeProps from '../../../utils/removeProps';
+
+export default styled(removeProps(Tag, ['isHref']))`
+  pointer-events: none;
+  ${p => (p.isHref && css`
+    pointer-events: auto;
+    cursor: pointer;
+    > a {
+      text-decoration: none;
+    }
+  `)}
+  > i {
+    pointer-events: auto;
+  }
+`;
