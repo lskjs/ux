785deadd9f824e6f136c82caaa1f38f32866c463
Андрей Чуриков <shitric2@gmail.com>
2019-03-06 21:46:57 +0400

    ПРОДУМЫВАЙТЕ СЕРВЕРНЫЙ РЕНДЕРИНГ

--- src/utils/isTouchDevice.js
+++ src/utils/isTouchDevice.js
@@ -1,6 +1,8 @@
 export default function isTouchDevice() {
-  if (!__CLIENT__) return false;
-  const el = document.createElement('div');
-  el.setAttribute('ongesturestart', 'return;');
-  return typeof el.ongesturestart === 'function';
+  if (typeof window !== 'undefined') {
+    const el = window.document.createElement('div');
+    el.setAttribute('ongesturestart', 'return;');
+    return typeof el.ongesturestart === 'function';
+  }
+  return false;
 }
