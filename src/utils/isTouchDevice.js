4bb4b3dc4ee2e5b09110f326293ebacfa7a081b8
Андрей Чуриков <shitric2@gmail.com>
2019-03-06 21:25:22 +0400

    __CLIENT__

--- src/utils/isTouchDevice.js
+++ src/utils/isTouchDevice.js
@@ -1,4 +1,5 @@
 export default function isTouchDevice() {
+  if (!__CLIENT__) return false;
   const el = document.createElement('div');
   el.setAttribute('ongesturestart', 'return;');
   return typeof el.ongesturestart === 'function';
