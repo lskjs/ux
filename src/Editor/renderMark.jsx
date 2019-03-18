9688e04eaeb5019b134e65e262a689f2ba9fabaf
Andrey Kondalov <i@yukio.ru>
2019-03-18 19:29:20 +0400

    Editor

--- src/Editor/renderMark.jsx
+++ src/Editor/renderMark.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default function renderMark(props, editor, next) {
   const { children, mark, attributes } = props;
+  console.log('mark', mark, children);
   switch (mark.type) {
     case 'bold':
       return <strong {...{ attributes }}>{children}</strong>;
@@ -12,6 +13,7 @@ export default function renderMark(props, editor, next) {
     case 'underline':
     case 'underlined':
       return <u {...{ attributes }}>{children}</u>;
+    case 'deleted':
     case 'strikethrough':
       return <strike {...{ attributes }}>{children}</strike>;
     default:
