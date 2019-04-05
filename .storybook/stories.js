8c5784370ae69db7243dd1dc8281d47b4c4513a3
kafkae <dark.revo4@mail.ru>
2019-04-05 13:21:19 +0300

    carousel

--- .storybook/stories.js
+++ .storybook/stories.js
@@ -1,15 +1,15 @@
-const req = require.context(
-  '../src',
-  true,
-  /^.*(?!UI\/).*.story.js|.story.js|.story.jsx|.story.jsx$/,
-)
+// const req = require.context(
+//   '../src',
+//   true,
+//   /^.*(?!UI\/).*.story.js|.story.js|.story.jsx|.story.jsx$/,
+// )
 
-export default req.keys().map(req);
+// export default req.keys().map(req);
 
-// export default {  
-//   'src/A/A.story.jsx': require('../src/A/A.story.jsx').default,
-//   'src/Avatar/Avatar.story.jsx': require('../src/Avatar/Avatar.story.jsx').default,
-//   'src/AvatarGroup/AvatarGroup.story.jsx': require('../src/AvatarGroup/AvatarGroup.story.jsx').default,
-//   'src/Button/Button.story.jsx': require('../src/Button/Button.story.jsx').default,
-//   'src/Progress/Progress.story.jsx': require('../src/Progress/Progress.story.jsx').default,
-// };
+export default {  
+  'src/A/A.story.jsx': require('../src/A/A.story.jsx').default,
+  'src/Carousel/Carousel.story.jsx': require('../src/Carousel/Carousel.story.jsx').default,
+  'src/AvatarGroup/AvatarGroup.story.jsx': require('../src/AvatarGroup/AvatarGroup.story.jsx').default,
+  'src/Button/Button.story.jsx': require('../src/Button/Button.story.jsx').default,
+  'src/Progress/Progress.story.jsx': require('../src/Progress/Progress.story.jsx').default,
+};
