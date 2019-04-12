ea228dff45053303a0be2ef2d3a052069ccb7ced
Igor Suvorov <hi@isuvorov.ru>
2019-04-12 18:37:58 +0300

    update lsk

--- packages/dev/.storybook/webpack.config.js
+++ packages/dev/.storybook/webpack.config.js
@@ -1,10 +1,11 @@
+const babelrc = require('../.babelrc.js');
 module.exports = async ({ config, mode }) => {
-
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve('babel-loader'),
   });
-
   config.resolve.extensions.push('.ts', '.tsx');
+  config.module.rules[0].use[0].options.plugins = babelrc.plugins;
+  // config.module.rules[0].use[0].options.plugins.push('emotion');
   return config;
 };
