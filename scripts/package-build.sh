fd15fea54b49634ddc326128a58e972b99230754
Andrey Kondalov <i@yukio.ru>
2019-04-15 16:07:10 +0400

    Fix styles build

--- scripts/package-build.sh
+++ scripts/package-build.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-mkdir -p ${DIST:-build} && \
-cp package.json ${DIST:-build}/ && \
-cp package-lock.json ${DIST:-build}/ && \
-cp README.md ${DIST:-build}/ && \
+mkdir -p ${DIST:-build}
+cp package.json ${DIST:-build}/
+cp package-lock.json ${DIST:-build}/
+cp README.md ${DIST:-build}/
+
+if [ -d "src/styles" ]
+then
+mkdir -p ${DIST:-build/styles}
+cp -R src/styles/lib ${DIST:-build/styles}/
+cp src/styles/index.g.css ${DIST:-build/styles}/
+fi
+
 # cp {package.json,package-lock.json,README.md} ${DIST:-build}/ && \
 # npx babel src --out-dir ${DIST:-build} --source-maps inline --copy-files ${BUILD_PARAMS}
 npx babel src --out-dir ${DIST:-build} --source-maps both --ignore .test.jsx? ${BUILD_PARAMS}
