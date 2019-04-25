b0a736e858f1ee87f8e1d3f33c52951e647ce579
Andrey Kondalov <i@yukio.ru>
2019-04-25 19:24:34 +0300

    Fix list getTheme

--- src/List/List.styles.js
+++ src/List/List.styles.js
@@ -1,17 +1,18 @@
 import styled, { css } from 'react-emotion';
+import getTheme from '@lskjs/theme/getTheme';
 import createDynamicTag from '../utils/createDynamicTag';
 import { Row } from '../Grid';
 // import TabBlock from '~/Uapp/components.v2/atoms/TabBlock';
 
 export const Title = styled('div')`
-  font-family: ${p => p.theme.fontFamily};
+  font-family: ${p => getTheme(p.theme, 'fontFamily')};
   font-size: 20px;
   font-weight: 500;
   font-style: normal;
   font-stretch: normal;
   line-height: 1.8;
   letter-spacing: -0.1px;
-  color: ${p => p.theme.colors.main};
+  color: ${p => getTheme(p.theme, 'colors.main')};
 `;
 
 export const SearchWrapper = styled('div')`
@@ -20,14 +21,14 @@ export const SearchWrapper = styled('div')`
   padding: 8px 12px;
   position: relative;
   border-radius: 4px;
-  background-color: ${p => p.theme.colors.white};
-  border: 1px solid ${p => p.theme.colors.border};
+  background-color: ${p => getTheme(p.theme, 'colors.white')};
+  border: 1px solid ${p => getTheme(p.theme, 'colors.border')};
   width: 240px;
   overflow: hidden;
 
   > svg {
     font-size: 24px;
-    color: ${p => p.theme.colors.primary};
+    color: ${p => getTheme(p.theme, 'colors.primary')};
   }
 `;
 
@@ -41,30 +42,30 @@ export const SearchInput = styled('input')`
   height: 100%;
   width: 100%;
   padding: 8px 12px 8px 44px;
-  font-family: ${p => p.theme.fontFamily};
+  font-family: ${p => getTheme(p.theme, 'fontFamily')};
   font-size: 13px;
   font-weight: normal;
   font-style: normal;
   font-stretch: normal;
   line-height: 1.54;
   letter-spacing: normal;
-  color: ${p => p.theme.colors.main};
+  color: ${p => getTheme(p.theme, 'colors.main')};
 
   ::-webkit-input-placeholder {
-    color: ${p => p.theme.colors.secondary};
-    font-family: ${p => p.theme.fontFamily};
+    color: ${p => getTheme(p.theme, 'colors.secondary')};
+    font-family: ${p => getTheme(p.theme, 'fontFamily')};
   }
   ::-moz-placeholder {
-    color: ${p => p.theme.colors.secondary};
-    font-family: ${p => p.theme.fontFamily};
+    color: ${p => getTheme(p.theme, 'colors.secondary')};
+    font-family: ${p => getTheme(p.theme, 'fontFamily')};
   }
   :-ms-input-placeholder {
-    color: ${p => p.theme.colors.secondary};
-    font-family: ${p => p.theme.fontFamily};
+    color: ${p => getTheme(p.theme, 'colors.secondary')};
+    font-family: ${p => getTheme(p.theme, 'fontFamily')};
   }
   :-moz-placeholder {
-    color: ${p => p.theme.colors.secondary};
-    font-family: ${p => p.theme.fontFamily};
+    color: ${p => getTheme(p.theme, 'colors.secondary')};
+    font-family: ${p => getTheme(p.theme, 'fontFamily')};
   }
 `;
 
@@ -73,7 +74,7 @@ export const ArrowWrapper = styled('div')`
   font-size: 13px;
   height: 14px;
   margin: 0 4px;
-  color: ${p => p.theme.colors.primary};
+  color: ${p => getTheme(p.theme, 'colors.primary')};
 
   transition: transform .2s ease, opacity .2s ease;
   will-change: transform, opacity;
@@ -93,9 +94,9 @@ export const ArrowWrapper = styled('div')`
 
 export const HeaderItemWrapper = styled('div')`
   height: 48px;
-  padding: 0 ${p => p.theme.tablePadding}px;
-  font-family: ${p => p.theme.fontFamily};
-  background-color: ${p => p.theme.colors.white};
+  padding: 0 ${p => getTheme(p.theme, 'tablePadding')}px;
+  font-family: ${p => getTheme(p.theme, 'fontFamily')};
+  background-color: ${p => getTheme(p.theme, 'colors.white')};
   font-weight: 500;
   > div > div {
     display: flex;
@@ -113,16 +114,16 @@ export const Wrapper = styled('div')`
   /* display: grid; */
   width: 100%;
   /* overflow: hidden; */
-  border-radius: ${p => p.theme.borderRadius};
-  border: 1px solid ${p => p.theme.colors.border};
-  background-color: ${p => p.theme.colors.white};
+  border-radius: ${p => getTheme(p.theme, 'borderRadius')};
+  border: 1px solid ${p => getTheme(p.theme, 'colors.border')};
+  background-color: ${p => getTheme(p.theme, 'colors.white')};
 
   > *:first-child {
-    border-radius: ${p => p.theme.borderRadius} ${p => p.theme.borderRadius} 0 0;
+    border-radius: ${p => getTheme(p.theme, 'borderRadius')} ${p => getTheme(p.theme, 'borderRadius')} 0 0;
   }
 
   > *:last-child {
-    border-radius: 0 0 ${p => p.theme.borderRadius} ${p => p.theme.borderRadius};
+    border-radius: 0 0 ${p => getTheme(p.theme, 'borderRadius')} ${p => getTheme(p.theme, 'borderRadius')};
   }
 `;
 
@@ -130,20 +131,20 @@ const dynamicListTableItemTag = createDynamicTag('div');
 export const ListTableItem = styled(dynamicListTableItemTag)`
   display: block;
   height: ${p => p.height}px;
-  padding: 0 ${p => p.theme.tablePadding}px;
-  background-color: ${p => p.theme.colors.white};
+  padding: 0 ${p => getTheme(p.theme, 'tablePadding')}px;
+  background-color: ${p => getTheme(p.theme, 'colors.white')};
 
   ${p => (p.clickable && css`
     cursor: pointer;
     transition: background-color .2s ease;
     will-change: background-color;
     &:hover {
-      background-color: ${p.theme.colors.lighterPrimary};
+      background-color: ${getTheme(p.theme, 'colors.lighterPrimary')};
     }
   `)}
 
   &:not(:last-child) {
-    border-bottom: 1px solid ${p => p.theme.colors.border};
+    border-bottom: 1px solid ${p => getTheme(p.theme, 'colors.border')};
   }
 `;
 
@@ -172,13 +173,13 @@ export const BodyWrapperPadded = styled(Row)`
 
 export const ItemsWrapper = styled('div')`
   > .table-gird-row {
-    padding: 0 ${p => p.theme.tablePadding}px;
+    padding: 0 ${p => getTheme(p.theme, 'tablePadding')}px;
   }
 `;
 
 export const FilterWrapper = styled('div')`
-  padding: 22px ${p => p.theme.tablePadding}px 0;
-  background-color: ${p => p.theme.colors.lightGray};
+  padding: 22px ${p => getTheme(p.theme, 'tablePadding')}px 0;
+  background-color: ${p => getTheme(p.theme, 'colors.lightGray')};
   position: relative;
   bottom: 1px;
   margin-top: 1px;
@@ -187,17 +188,17 @@ export const FilterWrapper = styled('div')`
 export const FooterWrapper = styled('div')`
   /* display: flex; */
   /* align-items: center; */
-  font-family: ${p => p.theme.fontFamily};
+  font-family: ${p => getTheme(p.theme, 'fontFamily')};
   font-size: 13px;
-  color: ${p => p.theme.colors.secondary};
+  color: ${p => getTheme(p.theme, 'colors.secondary')};
   /* background-color: ${p => p.theme.colors.darkerBackground}; */
-  border-top: 1px solid ${p => p.theme.colors.border};
+  border-top: 1px solid ${p => getTheme(p.theme, 'colors.border')};
   /* height: auto; */
-  padding: 8px ${p => p.theme.tablePadding}px;
+  padding: 8px ${p => getTheme(p.theme, 'tablePadding')}px;
   /* @media screen and (max-width: 1317px) {
     flex-direction: column;
     align-items: flex-end;
-    padding: 12px ${p => p.theme.tablePadding}px;
+    padding: 12px ${p => getTheme(p.theme, 'tablePadding')}px;
     height: auto;
   } */
 `;
@@ -212,7 +213,7 @@ export const PagesWrapper = styled('div')`
 
 export const PaginatorWrapper = styled('div')`
   /* font-family: ${p => p.theme.fontFamily}; */
-  color: ${p => p.theme.colors.secondary};
+  color: ${p => getTheme(p.theme, 'colors.secondary')};
 `;
 
 export const StepperWrapper = styled('div')`
@@ -229,10 +230,10 @@ export const SelectWrapper = styled('select')`
 
 export const HeaderWrapper = styled('div')`
   /* display: grid; */
-  background-color: ${p => p.theme.colors.white};
+  background-color: ${p => getTheme(p.theme, 'colors.white')};
 
   > * {
-    box-shadow: inset 0 -1px 0 ${p => p.theme.colors.border};
+    box-shadow: inset 0 -1px 0 ${p => getTheme(p.theme, 'colors.border')};
   }
 
 ${p => (p.sticky && css`
@@ -243,7 +244,7 @@ ${p => (p.sticky && css`
 `;
 
 export const TagsPanelWrapper = styled('div')`
-  padding: ${p => p.theme.tablePadding}px ${p => p.theme.tablePadding}px 4px;
+  padding: ${p => getTheme(p.theme, 'tablePadding')}px ${p => getTheme(p.theme, 'tablePadding')}px 4px;
   display: flex;
   flex-wrap: wrap;
   > button {
@@ -280,18 +281,18 @@ export const HoverRowWrapper = styled(hoverTag)`
   cursor: pointer;
   display: block;
   > .table-gird-row {
-    background-color: ${p => p.theme.colors.white};
+    background-color: ${p => getTheme(p.theme, 'colors.white')};
     transition: background-color .2s ease-out;
     will-change: background-color;
   }
   ${p => (p.bordered && css`
     &:not(:last-of-type) {
-      border-bottom: 1px solid ${p.theme.colors.border};
+      border-bottom: 1px solid ${getTheme(p.theme, 'colors.border')};
     }
   `)}
   &:hover {
     > .table-gird-row {
-      background-color: ${p => p.theme.colors.lighterPrimary};
+      background-color: ${p => getTheme(p.theme, 'colors.lighterPrimary')};
     }
   }
 `;
@@ -300,18 +301,18 @@ export const SelectRowWrapper = styled('div')`
   cursor: pointer;
   display: block;
   > .table-gird-row {
-    background-color: ${p => p.theme.colors.white};
+    background-color: ${p => getTheme(p.theme, 'colors.white')};
     transition: background-color .2s ease-out;
     will-change: background-color;
   }
   ${p => (p.bordered && css`
     &:not(:last-of-type) {
-      border-bottom: 1px solid ${p.theme.colors.border};
+      border-bottom: 1px solid ${getTheme(p.theme, 'colors.border')};
     }
   `)}
   ${p => (p.checked && css`
     > .table-gird-row {
-      background-color: ${p.theme.colors.lightPrimary};
+      background-color: ${getTheme(p.theme, 'colors.lightPrimary')};
     }
   `)}
 `;
@@ -321,5 +322,3 @@ export const modalStyle = css`
     display: block !important;
   }
 `;
-
-
