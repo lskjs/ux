a74674706c5f50b0f06f5f86ba913c34383ba227
Igor Suvorov <hi@isuvorov.ru>
2019-05-08 17:55:17 +0300

    updates

--- scripts/update-starter-kit.sh
+++ scripts/update-starter-kit.sh
@@ -1,4 +1,14 @@
+VAR=$(git status --porcelain 2>/dev/null | wc -l)
+echo $VAR
+if [ $VAR -gt 0 ]; then
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" && \
+    echo "Working tree has uncommitted changes ($VAR), please commit or remove changes before continuing." && \
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" && \
+    exit 1
+fi
+
 HOME='.'
+git pull ../lib-starter-kit
 rsync -avE --progress  ../lib-starter-kit/ $HOME --exclude='*/' --exclude='lerna.json' --exclude='.huskyrc.json' --exclude='README.md'
 rsync -avE --progress  ../lib-starter-kit/.storybook $HOME
 rsync -avE --progress  ../lib-starter-kit/scripts/* $HOME/scripts
