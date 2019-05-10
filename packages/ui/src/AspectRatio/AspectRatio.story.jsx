91aee33ad8de33d54a49fad7e12183b122e2e178
Andrey Kondalov <i@yukio.ru>
2019-05-10 19:30:08 +0400

    AspectRatio

---
+++ packages/ui/src/AspectRatio/AspectRatio.story.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Story from '@lskjs/dev/Story';
+import AspectRatio from './AspectRatio';
+
+export default ({ storiesOf }) => (
+  storiesOf('AspectRatio', module)
+    .add('Auto', () => (
+      <Story>
+        <AspectRatio>
+          <img width="100%" alt="" src="https://picsum.photos/800" />
+        </AspectRatio>
+      </Story>
+    ))
+    .add('1:1', () => (
+      <Story>
+        <AspectRatio ratio="1:1">
+          <img width="100%" alt="" src="https://picsum.photos/800" />
+        </AspectRatio>
+      </Story>
+    ))
+    .add('4:3', () => (
+      <Story>
+        <AspectRatio ratio="4:3">
+          <img width="100%" alt="" src="https://picsum.photos/800" />
+        </AspectRatio>
+      </Story>
+    ))
+    .add('16:9', () => (
+      <Story>
+        <AspectRatio ratio="16:9">
+          <img width="100%" alt="" src="https://picsum.photos/800" />
+        </AspectRatio>
+      </Story>
+    ))
+    .add('21:9', () => (
+      <Story>
+        <AspectRatio ratio="21:9">
+          <img width="100%" alt="" src="https://picsum.photos/800" />
+        </AspectRatio>
+      </Story>
+    ))
+);
---
+++ packages/ui/src/AspectRatio/AspectRatio.styles.js
@@ -0,0 +1,36 @@
+import styled from 'react-emotion';
+import removeProps from '@lskjs/utils/removeProps';
+
+function makeRatio(p) {
+  switch (p.ratio) {
+    case '1:1': return '100%';
+    case '4:3': return '75%';
+    case '16:9': return '56.25%';
+    case '21:9': return '42.857143%';
+    default: return 'auto';
+  }
+}
+const filteredTag = removeProps('div', ['ratio']);
+export const Wrapper = styled(filteredTag)`
+  position: relative;
+  display: block;
+  width: 100%;
+  padding: 0;
+  overflow: hidden;
+
+  &:before {
+    display: block;
+    content: '';
+    padding-top: ${makeRatio};
+  }
+`;
+
+export const Inner = styled('div')`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+`;
