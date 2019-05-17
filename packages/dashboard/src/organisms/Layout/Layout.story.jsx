6b9d7fc0239dfaddf95c41b212c9fa01d9750fbc
Igor Suvorov <hi@isuvorov.ru>
2019-05-17 19:32:15 +0300

    remove g. prefix

--- packages/dashboard/src/organisms/Layout/Layout.story.jsx
+++ packages/dashboard/src/organisms/Layout/Layout.story.jsx
@@ -27,7 +27,7 @@ import LayoutHeader from '../../molecules/LayoutHeader';
 import LayoutSidebar from '../../molecules/LayoutSidebar';
 import ListItem from '../../molecules/ListItem';
 
-import '@lskjs/css/lib/bootstrap.g.css';
+import '@lskjs/css/lib/bootstrap.css';
 // import '../../../styles/lib/bootstrap.g.css';
 
 import Layout from './Layout';
