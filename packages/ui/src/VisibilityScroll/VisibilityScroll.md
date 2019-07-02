476ff428854e8b8bf82df85a7afab2e131a6297d
kafkae <villas1998@mail.ru>
2019-07-02 18:23:47 +0400

    BGUI-56  Создал сторибук и документацию к блоку VisibilityScroll

---
+++ packages/ui/src/VisibilityScroll/VisibilityScroll.md
@@ -0,0 +1,17 @@
+Стандартный вид компонента
+```jsx
+  <div style={{ minHeight: '2000px' }}>
+    <VisibilityScroll trigger={25}>
+      <div style={{ border: '2px solid red', position: 'fixed', bottom: '20px', right: '20px' }}>Содержимое</div>
+    </VisibilityScroll>
+  </div>
+```
+
+Вид компонента без title
+```jsx
+  <div style={{ minHeight: '2000px' }}>
+    <VisibilityScroll trigger={55}>
+      <div style={{ border: '2px solid red', position: 'fixed', bottom: '20px', right: '20px' }}>Содержимое</div>
+    </VisibilityScroll>
+  </div>
+```
---
+++ packages/ui/src/VisibilityScroll/VisibilityScroll.story.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import range from 'lodash/range';
+import VisibilityScroll from './VisibilityScroll';
+import Story from '@lskjs/dev/Story';
+
+
+export default ({ storiesOf }) => (
+  storiesOf('VisibilityScroll', module)
+    .add('default', () => (
+      <Story>
+        <div style={{ minHeight: '2000px' }}>
+          <VisibilityScroll trigger={25}>
+            <div style={{ border: '2px solid red', position: 'fixed', bottom: '20px', right: '20px' }}>Содержимое</div>
+          </VisibilityScroll>
+        </div>
+      </Story>
+    ))
+    .add('another value trigger', () => (
+      <Story>
+        <div style={{ minHeight: '2000px' }}>
+          <VisibilityScroll trigger={55}>
+            <div style={{ border: '2px solid red', position: 'fixed', bottom: '20px', right: '20px' }}>Содержимое</div>
+          </VisibilityScroll>
+        </div>
+      </Story>
+    ))
+);
