75db7999ac422d93d16ee33885b268e3287c01e6
kafkae <villas1998@mail.ru>
2019-07-02 19:18:51 +0400

    BGUI-57  Создал сторибук и документацию к блоку BackToTop

---
+++ packages/ui/src/BackToTop/BackToTop.md
@@ -0,0 +1,69 @@
+Стандартный вид компонента
+```jsx
+ <div style={{ minHeight: '2000px' }}>
+  <BackToTop />
+</div>
+```
+
+Другой цвет состояния
+```jsx
+ <div style={{ minHeight: '2000px' }}>
+  <BackToTop paint="warning" />
+</div>
+```
+
+без фиксированного положения
+```jsx
+ <div style={{ minHeight: '2000px' }}>
+    <BackToTop fixed={false} />
+  </div>
+```
+
+position top left
+```jsx
+ <div style={{ minHeight: '2000px' }}>
+    <BackToTop position="top left" />
+  </div>
+```
+
+position top right
+```jsx
+<div style={{ minHeight: '2000px' }}>
+  <BackToTop position="top right" />
+</div>
+```
+
+position right top
+```jsx
+<div style={{ minHeight: '2000px' }}>
+  <BackToTop position="right top" />
+</div>
+```
+
+position bottom left
+```jsx
+<div style={{ minHeight: '2000px' }}>
+  <BackToTop position="bottom left" />
+</div>
+```
+
+position bottom right
+```jsx
+<div style={{ minHeight: '2000px' }}>
+  <BackToTop position="bottom right" />
+</div>
+```
+
+position right bottom
+```jsx
+<div style={{ minHeight: '2000px' }}>
+  <BackToTop position="right bottom" />
+</div>
+```
+
+position left bottom
+```jsx
+<div style={{ minHeight: '2000px' }}>
+  <BackToTop position="left bottom" />
+</div>
+```
---
+++ packages/ui/src/BackToTop/BackToTop.story.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import BackToTop from './BackToTop';
+import Story from '@lskjs/dev/Story';
+
+export default ({ storiesOf }) => (
+  storiesOf('BackToTop', module)
+    .add('default', () => (
+      <Story>
+        <div style={{ minHeight: '2000px' }}>
+          <BackToTop />
+        </div>
+      </Story>
+    ))
+    .add('another color', () => (
+      <Story>
+        <div style={{ minHeight: '2000px' }}>
+          <BackToTop paint="warning" />
+        </div>
+      </Story>
+    ))
+    .add('without fixed', () => (
+      <Story>
+        <div style={{ minHeight: '2000px' }}>
+          <BackToTop fixed={false} />
+        </div>
+      </Story>
+    ))
+    .add('position top left', () => (
+      <Story>
+        <div style={{ minHeight: '2000px' }}>
+          <BackToTop position="top left" />
+        </div>
+      </Story>
+    ))
+    .add('position top right', () => (
+      <Story>
+        <div style={{ minHeight: '2000px' }}>
+          <BackToTop position="top right" />
+        </div>
+      </Story>
+    ))
+    .add('position right top', () => (
+      <Story>
+        <div style={{ minHeight: '2000px' }}>
+          <BackToTop position="right top" />
+        </div>
+      </Story>
+    ))
+    .add('position bottom left', () => (
+      <Story>
+        <div style={{ minHeight: '2000px' }}>
+          <BackToTop position="bottom left" />
+        </div>
+      </Story>
+    ))
+    .add('position bottom right', () => (
+      <Story>
+        <div style={{ minHeight: '2000px' }}>
+          <BackToTop position="bottom right" />
+        </div>
+      </Story>
+    ))
+    .add('position right bottom', () => (
+      <Story>
+        <div style={{ minHeight: '2000px' }}>
+          <BackToTop position="right bottom" />
+        </div>
+      </Story>
+    ))
+    .add('position left bottom', () => (
+      <Story>
+        <div style={{ minHeight: '2000px' }}>
+          <BackToTop position="left bottom" />
+        </div>
+      </Story>
+    ))
+);
