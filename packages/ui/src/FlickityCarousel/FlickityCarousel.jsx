777969c9619e4a5a3b8f152aad0cffefb1d1e8e1
Александр Можаев <nadymorigin@gmail.com>
2019-07-25 19:34:39 +0400

    FlickityCarousel fix

--- packages/ui/src/FlickityCarousel/FlickityCarousel.jsx
+++ packages/ui/src/FlickityCarousel/FlickityCarousel.jsx
@@ -1,34 +1,54 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import Button from '@lskjs/button';
 import Flickity from 'react-flickity-component';
+import { prevButton, flickityStyle } from './FlickityCarousel.style';
+import CarouselButton from './assets/carousel-go';
+
+const flickityOptions = {
+  initialIndex: 0,
+  pageDots: false,
+  prevNextButtons: false,
+  cellAlign: 'left',
+  contain: true,
+};
 
 class FlickityCarousel extends PureComponent {
+  static propTypes = {
+    children: PropTypes.any,
+  }
+  static defaultProps = {
+    children: null,
+  }
+  constructor(props) {
+    super(props);
+    this.flkty = React.createRef();
+  }
   myCustomNext = () => {
-    // You can use Flickity API
     this.flkty.next();
   }
-  myCustomPrev = () => {
-    // You can use Flickity API
-    this.flkty.prev();
+  myCustomPrevious = () => {
+    this.flkty.previous();
   }
   render() {
     const { children, ...props } = this.props;
     return (
       <div>
         <Flickity
-          className="carousel" // default ""
+          // className="carousel" // default ""
           elementType="div"// default 'div'
           disableImagesLoaded={false} // default false
           reloadOnUpdate // default false
+          options={flickityOptions}
           static // default false
-          pageDots={false}
           flickityRef={c => this.flkty = c}
+          className={flickityStyle}
           {...props}
         >
           {children}
         </Flickity>
-        <Button paint="primary" onClick={this.myCustomNext}>Next button</Button>
-        <Button paint="danger" onClick={this.myCustomPrev}>Prev button</Button>
+        <Button paint="primary" className={prevButton} icon={<CarouselButton />} onClick={this.myCustomPrevious} />
+        <Button paint="primary" icon={<CarouselButton />} onClick={this.myCustomNext} />
       </div>
     );
   }
---
+++ packages/ui/src/FlickityCarousel/FlickityCarousel.style.js
@@ -0,0 +1,11 @@
+import { css } from 'react-emotion';
+
+export const flickityStyle = css`
+  &:focus {
+    outline: none !important;
+  }
+`;
+
+export const prevButton = css`
+  transform: rotate(180deg);
+`;
