20fb850c2e573d476cc24abe35b71f8c44adcfee
Александр Можаев <nadymorigin@gmail.com>
2019-07-26 16:04:58 +0400

    fix carousel

--- packages/ui/src/FlickityCarousel/FlickityCarousel.styles.js
+++ packages/ui/src/FlickityCarousel/FlickityCarousel.styles.js
@@ -15,9 +15,7 @@ export const Control = styled('div')`
     transform: rotate(180deg);
   `)}
   ${p => (p.position === 'right' && css`right: 0;`)}
-  pointer-events: ${p => (p.visible ? 'auto' : 'none')};
   opacity: ${p => (p.visible ? 1 : 0.5)};
-  /* visibility: ${p => (p.visible ? 'visible' : 'hidden')}; */
   position: absolute;
   display: flex;
   align-items: center;
