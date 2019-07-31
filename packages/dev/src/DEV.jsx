9c4c8c272b0d0885a6f16b087e910569d15857d7
Andrey Kondalov <i@yukio.ru>
2019-07-31 14:54:36 +0400

    stylizing DEV

--- packages/dev/src/DEV.jsx
+++ packages/dev/src/DEV.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 const isDev = () => !!(typeof __DEV__ !== 'undefined' ? __DEV__ : global.__DEV__); // eslint-disable-line
 
-const DEV = ({ children, json, pretty = true }) => ( // eslint-disable-line
+const DEV = ({ children, json, pretty = true }) => (
   isDev() ? (
-    <div style={{ outline: '1px dotted black' }}>
+    <div
+      data-label="DEV"
+      data-dev={isDev()}
+      style={{
+        background: 'repeating-linear-gradient(45deg,#b5281d30,#b5281d30 10px,#f0413430 0,#f0413430 20px)',
+        outline: '1px dashed #ff0000b0',
+      }}
+    >
       {json
         ? (
           <pre>
