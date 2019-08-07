97c10f64a846e85e019c1bad9f4efe3e920329ca
Nikita Savyolov <ga2mer@yandex.ru>
2019-08-07 16:42:44 +0400

    downloads

--- packages/downloads/src/download.js
+++ packages/downloads/src/download.js
@@ -1,4 +1,26 @@
 import XlsxPopulate from 'xlsx-populate/browser/xlsx-populate';
+import zip from 'lodash/zip';
+
+function promiseMapSeries(array, iterator, thisArg) {
+  var length = array.length
+  var current = Promise.resolve()
+  var results = new Array(length)
+  var cb = arguments.length > 2 ? iterator.bind(thisArg) : iterator
+
+  for (var i = 0; i < length; ++i) {
+    current = results[i] = current.then(function(i) {
+      return cb(array[i], i, array)
+    }.bind(undefined, i))
+  }
+
+  return Promise.all(results)
+}
+
+const markupToMatrix = ({ markup, items, props }) => {
+  return zip(...markup.map(([title, fn]) => {
+    return [title, ...items.map(el => fn({ item: el, ...props }))];
+  }));
+};
 
 export const generateFromJSON = async (json) => {
   const workbook = await XlsxPopulate.fromBlankAsync();
@@ -24,10 +46,38 @@ export const downloadFile = async (workbook, name = 'filename') => {
   }
 };
 
-export default async ({ type = 'xlsx', name = 'filename', data }) => {
+export const generateFile = async ({ type = 'xlsx', name = 'filename', data }) => {
   if (type === 'xlsx') {
-    const workbook = await generateFromJSON(data, name);
-    downloadFile(workbook);
+    const workbook = await generateFromJSON(data);
+    downloadFile(workbook, name);
   }
 };
 
+const downloadFromList = (listStore) => {
+  return listStore.items;
+};
+
+const downloadAllFromList = async ({ listStore, limit, maxCount }) => {
+  const listStoreAll = new listStore.constructor(listStore);
+  listStoreAll.skip = 0;
+  listStoreAll.items = [];
+  const maxItemsCount = listStoreAll.count > maxCount ? maxCount : listStoreAll.count;
+  await promiseMapSeries(Array(Math.round(maxItemsCount / limit)).fill(), async () => {
+    await listStoreAll.fetchMore(1, limit);
+  });
+  return listStoreAll.items;
+};
+
+const download = async ({
+  listStore, limit = 50, maxCount = 10000, markup = [], markupProps = {}, name, all = false,
+}) => {
+  const items = await (all ? downloadAllFromList({ listStore, limit, maxCount }) : downloadFromList(listStore));
+  const data = markupToMatrix({
+    markup,
+    props: markupProps,
+    items,
+  });
+  generateFile({ name, data });
+};
+
+export default download;
--- packages/downloads/src/download.story.jsx
+++ packages/downloads/src/download.story.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
-import download from './download';
+import download, { generateFile } from './download';
 import Story from '@lskjs/dev/Story';
 import DEV from '@lskjs/dev/DEV';
+import StatefulButton from '@lskjs/button/StatefulButton';
+import Button from '@lskjs/button/Button';
+import ListStore from '@lskjs/mobx/stores/ListStore';
 
-const data = [
+const data = Array(500).fill().map((_, i) => ({
+  index: i,
+  field1: 'value1',
+  field2: 'value2',
+  field3: 'value3',
+  field4: 'value4',
+  field5: 'value5',
+  field6: 'value6',
+  field7: 'value7',
+  field8: 'value8',
+  field9: 'value9',
+}));
+
+const indexToValue = (fieldName) => {
+  return ({ item }) => {
+    return `${item[fieldName]} ${item.index}`;
+  };
+};
+
+const markup = [
+  ['field 1', indexToValue('field1')],
+  ['field 2', indexToValue('field2')],
+  ['field 3', indexToValue('field3')],
+  ['field 4', indexToValue('field4')],
+  ['field 5', indexToValue('field5')],
+  ['field 6', indexToValue('field6')],
+  ['field 7', indexToValue('field7')],
+  ['field 8', indexToValue('field8')],
+  ['field 9', indexToValue('field9')],
+];
+
+const markupProps = {};
+
+const api = {
+  async find({ skip = 0, limit = 20 } = {}) {
+    const count = data.length;
+    return {
+      count,
+      items: data.slice(skip, skip + limit),
+    };
+  },
+};
+
+const listStore = new ListStore({
+  api,
+});
+
+listStore.fetch({ limit: 25 });
+
+const defaultData = [
   ['title1', 'title2', 'title 3'],
   ['cell1', 'cell2', 'cell3'],
   ['cell4', 'cell5', 'cell6'],
@@ -13,10 +65,35 @@ export default ({ storiesOf }) => (
   storiesOf('downloads/utils', module)
     .add('download', () => (
       <Story>
-        <DEV json={data} />
-        <button onClick={() => download({ name: 'file', data })}>
+        <DEV json={defaultData} />
+        <button onClick={() => generateFile({ name: 'file', data: defaultData })}>
           Download spreadsheet
         </button>
+      </Story>
+    )).add('download button', () => (
+      <Story>
+        <StatefulButton
+          componentClass={Button}
+          paint="primary"
+          onClick={() => download({
+            listStore, markup, markupProps, name: 'default',
+          })}
+        >
+          Download
+        </StatefulButton>
+      </Story>
+    ))
+    .add('download button all', () => (
+      <Story>
+        <StatefulButton
+          componentClass={Button}
+          paint="primary"
+          onClick={() => download({
+            listStore, markup, markupProps, name: 'all', all: true,
+          })}
+        >
+          Download all
+        </StatefulButton>
       </Story>
     ))
 );
