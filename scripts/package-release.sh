af9f4d901e01658004f6e5fc47c1a4d50c915d5f
Andrey Kondalov <i@yukio.ru>
2019-09-16 16:14:14 +0400

    revert

--- scripts/package-release.sh
+++ scripts/package-release.sh
@@ -1,5 +1,3 @@
-# rm -rf release && \
-# DEBUG=0 DIST=release `dirname "$0"`/package-build.sh && \
-# npm publish release
-
-echo `dirname "$0"`
+rm -rf release && \
+DEBUG=0 DIST=release `dirname "$0"`/package-build.sh && \
+npm publish release
