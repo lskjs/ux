2139fa9236bc66b1b8814cc5664c75716703fcf4
Maxim Bureev <iburey@ya.ru>
2019-10-02 17:12:43 +0400

    fix docs

--- packages/docs/src/Story.story.jsx
+++ packages/docs/src/Story.story.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import Story from '@buzzguru/ui/Story';
 
 export default ({ storiesOf }) => (
   storiesOf('Story', module)
     .add('default', () => (
-      <Story>
-        <div>
-          test
-        </div>
-      </Story>
+      <div>
+        test
+      </div>
     ))
 );
