9cc6594890a7d7ce008ccbd543d3ea96b5c5d094
Igor Suvorov <hi@isuvorov.com>
2020-01-21 00:23:46 +0200

    quick fix

--- packages/form/src/controls/PhoneInput/react-phone-input-2/index.js
+++ packages/form/src/controls/PhoneInput/react-phone-input-2/index.js
@@ -1,5 +1,10 @@
 if (typeof window !== 'undefined') {
-  module.exports = require('react-phone-input-2').default;
+  try {
+    module.exports = require('react-phone-input-2').default;
+  } catch (err) {
+    console.error(err);
+    module.exports = () => null;
+  }
 } else {
   module.exports = () => null;
 }
