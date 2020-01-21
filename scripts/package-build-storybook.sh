52bbce332b9c6c85ece40e68de9d8d86b96a3c52
Maxim Bureev <iburey@ya.ru>
2020-01-21 11:37:43 +0400

    fix?

--- scripts/package-build-storybook.sh
+++ scripts/package-build-storybook.sh
@@ -3,4 +3,5 @@ env NODE_OPTIONS=--max_old_space_size=4096 \
   node ../../node_modules/@storybook/react/bin/build.js \
     -c .storybook \
     -o ../../docs
-git add .
+git add ../../
+git commit -m "docs"
