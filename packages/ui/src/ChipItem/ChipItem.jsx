369012518374d69f1579034497ee2816f6dd0d6b
Igor Suvorov <hi@isuvorov.com>
2020-02-06 21:47:15 +0200

    feat: add Chip component

---
+++ packages/ui/src/ChipItem/ChipItem.jsx
@@ -0,0 +1,45 @@
+
+import React from 'react';
+import Avatar from '../Avatar';
+import Link from '../Link';
+import Sider from '../Sider';
+
+import { ImageWrapper, TextWrapper } from './ChipItem.styles';
+
+const ChipItem = ({ link, item = {}, size = 40, children: content }) => {
+  const children = (
+    <Sider
+      verticalAlign="center"
+      gap={12}
+      left={(
+        <ImageWrapper>
+          <Avatar
+            id={item.id || item._id}
+            name={item.title || item.name}
+            src={item.image || item.avatar}
+            size={size}
+            style={{ flexShrink: 0 }}
+          />
+        </ImageWrapper>
+      )}
+      // style={{ marginRight: 10, padding: '5px 0' }}
+      right={(
+        <TextWrapper>
+          {item.title || item.name}
+          <div>
+            {content}
+          </div>
+        </TextWrapper>
+      )}
+    />
+  );
+
+  if (!link) return children;
+  return (
+    <Link href={item.link}>
+      {children}
+    </Link>
+  );
+};
+
+export default ChipItem;
---
+++ packages/ui/src/ChipItem/ChipItem.md
@@ -0,0 +1,11 @@
+ChipItem example:
+
+```js
+<ChipItem
+  item={{
+    id: random(99),
+    title: 'John Smith',
+    src: `https://randomuser.me/api/portraits/men/${random(99)}.jpg`,
+  }}
+/>
+```
