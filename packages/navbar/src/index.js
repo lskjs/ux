d8a28ad98da1ca3edf38cfccd3d1d6aa2e6e02b6
Igor Suvorov <hi@isuvorov.com>
2020-02-18 22:20:33 +0200

    fix: error while build

--- packages/navbar/src/index.js
+++ packages/navbar/src/index.js
@@ -1,13 +1,28 @@
-export Collapse from './Collapse';
-export Dropdown from './Dropdown';
-export DropdownButton from './DropdownButton';
-export DropdownItem from './DropdownItem';
+import Collapse from './Collapse';
+import Dropdown from './Dropdown';
+import DropdownButton from './DropdownButton';
+import DropdownItem from './DropdownItem';
 
-export Nav from './Nav';
-export Navbar from './Navbar';
-export NavbarBrand from './NavbarBrand';
-export NavDropdown from './NavDropdown';
-export NavItem from './NavItem';
-export NavLink from './NavLink';
-export SafeAnchor from './SafeAnchor';
-export ThemeProvider from './ThemeProvider';
+import Nav from './Nav';
+import Navbar from './Navbar';
+import NavbarBrand from './NavbarBrand';
+import NavDropdown from './NavDropdown';
+import NavItem from './NavItem';
+import NavLink from './NavLink';
+import SafeAnchor from './SafeAnchor';
+import ThemeProvider from './ThemeProvider';
+
+export {
+  Collapse,
+  Dropdown,
+  DropdownButton,
+  DropdownItem,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavDropdown,
+  NavItem,
+  NavLink,
+  SafeAnchor,
+  ThemeProvider,
+};
