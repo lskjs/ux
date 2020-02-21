fc5572287383bd6977e64da3759dc7a2c2ca4a2b
Igor Suvorov <hi@isuvorov.com>
2020-02-21 20:33:42 +0200

    fix: story fix

--- packages/form/src/stories/FormGroup.story.jsx
+++ packages/form/src/stories/FormGroup.story.jsx
@@ -124,7 +124,7 @@ export default ({ storiesOf }) =>
     })
     .add('FormGroup', () => {
       return (
-        <Story>ls
+        <Story>
           <FormGroupForm />
         </Story>
       );
