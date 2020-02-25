562b29fa002bb0000e6ab8b329a2244561f640e2
Igor Suvorov <hi@isuvorov.com>
2020-02-25 16:04:46 +0200

    refactor: some components

--- packages/ui/src/NotificationSystem/Notification/components/NotifyItem/NotifyItem.jsx
+++ packages/ui/src/NotificationSystem/Notification/components/NotifyItem/NotifyItem.jsx
@@ -1,37 +1,21 @@
-import React, { PureComponent } from 'react';
+import Reactfrom 'react';
 import PropTypes from 'prop-types';
 import Link from '@lskjs/link';
 import { Right, Item } from './NotifyItem.styles';
 
-
-class NotifyItem extends PureComponent {
-  static proPropTypesypes = {
-    unread: PropTypes.bool,
-    children: PropTypes.any.isRequired,
-    href: PropTypes.string,
-    type: PropTypes.string.isRequired,
-  }
-  static defaultProps = {
-    unread: false,
-  }
-  render() {
-    const {
-      href, unread, children, type,
-    } = this.props;
-    const Tag = href ? Link : 'div';
-    return (
-      <Item
-        componentClass={Tag}
-        href={href}
-        unread={unread}
-        type={type}
-      >
-        <Right>
-          {children}
-        </Right>
-      </Item>
-    );
-  }
-}
-
+const NotifyItem = ({ href, unread, children, type }) => (
+  <Item componentClass={href ? Link : 'div'} href={href} unread={unread} type={type}>
+    <Right>{children}</Right>
+  </Item>
+);
+NotifyItem.propTypes = {
+  unread: PropTypes.bool,
+  children: PropTypes.any.isRequired,
+  href: PropTypes.string,
+  type: PropTypes.string.isRequired,
+};
+NotifyItem.defaultProps = {
+  unread: false,
+  href: null,
+};
 export default NotifyItem;
