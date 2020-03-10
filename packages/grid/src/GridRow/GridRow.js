70b310557e289f491b92ba2ba330e051c2aa5b4a
Andrey Kondalov <i@yukio.ru>
2020-03-10 15:54:23 +0400

    refactor(grid): shouldForwardProp instead removeProps

--- packages/grid/src/GridRow/GridRow.js
+++ packages/grid/src/GridRow/GridRow.js
@@ -1,30 +1,31 @@
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
 import getTheme from '@lskjs/theme/getTheme';
-import removeProps from '@lskjs/utils/removeProps';
-import GridCol from '../GridCol';
 
-const filter = removeProps('div', ['vertical', 'gap']);
-export default styled(filter)`
+export default styled('div', { shouldForwardProp: prop => !['vertical', 'gap'].includes(prop) })`
   box-sizing: border-box;
   display: flex;
   flex-wrap: wrap;
-  margin-right: -${p => (p.gap || getTheme(p.theme, 'gridGap'))}px;
-  margin-left: -${p => (p.gap || getTheme(p.theme, 'gridGap'))}px;
+  margin-right: -${p => p.gap || getTheme(p.theme, 'gridGap')}px;
+  margin-left: -${p => p.gap || getTheme(p.theme, 'gridGap')}px;
 
-  ${p => p.gap && css`
-    > div {
+  ${p =>
+    p.gap &&
+    css`
+      > div {
         padding-left: ${p.gap || getTheme(p.theme, 'gridGap')}px;
         padding-right: ${p.gap || getTheme(p.theme, 'gridGap')}px;
-    }
-  `}
+      }
+    `}
 
-  ${p => (p.vertical && css`
-    margin-top: -${p.gap || getTheme(p.theme, 'gridGap')}px;
-    margin-bottom: -${p.gap || getTheme(p.theme, 'gridGap')}px;
-    > div {
-      padding-top: ${p.gap || getTheme(p.theme, 'gridGap')}px;
-      padding-bottom: ${p.gap || getTheme(p.theme, 'gridGap')}px;
-    }
-  `)}
+  ${p =>
+    p.vertical &&
+    css`
+      margin-top: -${p.gap || getTheme(p.theme, 'gridGap')}px;
+      margin-bottom: -${p.gap || getTheme(p.theme, 'gridGap')}px;
+      > div {
+        padding-top: ${p.gap || getTheme(p.theme, 'gridGap')}px;
+        padding-bottom: ${p.gap || getTheme(p.theme, 'gridGap')}px;
+      }
+    `}
 `;
