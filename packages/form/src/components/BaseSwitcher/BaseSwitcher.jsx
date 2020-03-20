d15297f11293c7fa54464bf76dae8f3a6f84d54e
Александр Можаев <nadymorigin@gmail.com>
2020-03-20 18:06:04 +0400

    feat: add prop left & right to BaseSwitcher

--- packages/form/src/components/BaseSwitcher/BaseSwitcher.jsx
+++ packages/form/src/components/BaseSwitcher/BaseSwitcher.jsx
@@ -1,45 +1,31 @@
 import React, { PureComponent } from 'react';
+import If from 'react-if';
 import autobind from '@lskjs/autobind';
 import PropTypes from 'prop-types';
-import {
-  Label,
-  Handler,
-  Block,
-  Wrap,
-} from './BaseSwitcher.styles';
+import { Label, Handler, Block, Wrap, LabelWrapper } from './BaseSwitcher.styles';
 
 class BaseSwitcher extends PureComponent {
-  static propTypes = {
-    value: PropTypes.bool,
-    onChange: PropTypes.func,
-    validationState: PropTypes.oneOf(['success', 'warning', 'error']),
-  }
-  static defaultProps = {
-    value: false,
-    onChange: null,
-    validationState: null,
-  }
   @autobind
   handleChange() {
     const { onChange, value } = this.props;
     if (onChange) onChange(!value);
   }
   render() {
-    const {
-      value, label, checkedTitle, reversed, styleSwitcher, disabled, validationState,
-    } = this.props;
+    const { value, label, checkedTitle, reversed, styleSwitcher, disabled, validationState, left, right } = this.props;
     // let text = t(`common.switch.${value ? 'on' : 'off'}`);
     let text = null;
     if (label || checkedTitle) {
-      text = (value && checkedTitle) ? checkedTitle : label;
+      text = value && checkedTitle ? checkedTitle : label;
     }
+    const double = left || right;
     return (
       <Wrap disabled={disabled}>
-        {!reversed &&
-        <div>
-          {text && <Label>{text}</Label>}
-        </div>
-        }
+        <If condition={!!left}>
+          <LabelWrapper left checked={!value}>
+            {left}
+          </LabelWrapper>
+        </If>
+        {!reversed && !double && <div>{text && <Label>{text}</Label>}</div>}
         <Block
           type="button"
           checked={value}
@@ -50,14 +36,32 @@ class BaseSwitcher extends PureComponent {
         >
           <Handler />
         </Block>
-        {reversed &&
-        <div>
-          {text && <Label>{text}</Label>}
-        </div>
-        }
+        {reversed && !double && <div>{text && <Label>{text}</Label>}</div>}
+        <If condition={!!right}>
+          <LabelWrapper right checked={value}>
+            {right}
+          </LabelWrapper>
+        </If>
       </Wrap>
     );
   }
 }
+
+BaseSwitcher.propTypes = {
+  value: PropTypes.bool,
+  onChange: PropTypes.func,
+  validationState: PropTypes.oneOf(['success', 'warning', 'error']),
+  // eslint-disable-next-line react/forbid-prop-types
+  left: PropTypes.any,
+  // eslint-disable-next-line react/forbid-prop-types
+  right: PropTypes.any,
+};
+BaseSwitcher.defaultProps = {
+  value: false,
+  onChange: null,
+  validationState: null,
+  left: null,
+  right: null,
+};
 
 export default BaseSwitcher;
--- packages/form/src/components/BaseSwitcher/BaseSwitcher.styles.js
+++ packages/form/src/components/BaseSwitcher/BaseSwitcher.styles.js
@@ -21,7 +21,7 @@ export const Handler = styled('div')`
   border-radius: 50px;
   margin-left: 0;
 
-  transition: margin-left .8s ease;
+  transition: margin-left 0.8s ease;
   will-change: margin-left;
 `;
 
@@ -40,24 +40,30 @@ export const Block = styled('button')`
   background-color: ${p => getTheme(p.theme, 'colors.border')};
   padding: 4px;
 
-  transition: background-color .2s ease;
-  ${p => (p.validationState === 'error' && `
+  transition: background-color 0.2s ease;
+  ${p =>
+    p.validationState === 'error' &&
+    `
     background-color: ${getTheme(p.theme, 'colors.danger')};
-  `)}
+  `}
   will-change: background-color;
-  ${p => (p.checked && `
+  ${p =>
+    p.checked &&
+    `
     background-color: ${getTheme(p.theme, 'colors.primary')};
     ${Handler} {
       margin-left: auto;
     }
-  `)}
+  `}
 `;
 
 export const Wrap = styled('div')`
   display: flex;
   align-items: center;
   flex-shrink: 0;
-  ${p => (p.disabled && `
+  ${p =>
+    p.disabled &&
+    `
     ${Block} {
       background-color: ${getTheme(p.theme, 'colors.border')} !important;
       cursor: not-allowed;
@@ -65,5 +71,19 @@ export const Wrap = styled('div')`
     ${Label} {
       color: ${getTheme(p.theme, 'colors.border')};
     }
-  `)}
+  `}
+`;
+
+export const LabelWrapper = styled('div')`
+  * {
+    color: ${p => p.checked && getTheme(p.theme, 'colors.primary')} !important;
+  }
+  ${p =>
+    p.left &&
+    `margin-right: 12px;
+  `};
+  ${p =>
+    p.right &&
+    `margin-left: 12px;
+  `};
 `;
