f86f6e1d7e38f013b7d5ad47a1edd39d1702cd4d
Igor Suvorov <hi@isuvorov.com>
2020-04-14 20:32:51 +0300

    feat(flag): add @lskjs/flag package

---
+++ packages/flag/src/Flag.story.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Story from '@lskjs/dev/Story';
+import Flag from './Flag';
+
+export default ({ storiesOf }) =>
+  storiesOf('ui/Flag', module)
+    .add('code', () => (
+      <Story>
+        <Flag />
+      </Story>
+    ))
+    .add('custom height', () => (
+      <Story>
+        <Flag height={55} />
+      </Story>
+    ))
+    .add('another country', () => (
+      <Story>
+        <Flag country="fr" />
+      </Story>
+    ))
+    .add('more', () => (
+      <Story>
+        <Flag country="FR" />
+        <Flag country="uk" />
+        <Flag country="BL" />
+        <Flag country="ru" />
+      </Story>
+    ));
