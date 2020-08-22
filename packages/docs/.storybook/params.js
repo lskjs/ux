a23c852c825a22e0408d82e3131a796ca47f34b3
Andrey Kondalov <i@yukio.ru>
2020-08-22 18:20:42 +0400

    fix(docs): context is back

--- packages/docs/.storybook/params.js
+++ packages/docs/.storybook/params.js
@@ -1,13 +1,9 @@
-const req = require.context(
-  '../src',
-  true,
-  /.story.js|.story.js|.story.jsx|.story.jsx|.story.tsx$/,
-);
+import contexts from './contexts';
 
 export default {
-  modules: req.keys().map(req),
+  modules: [].concat.apply([], contexts.map((req) => req.keys().map(req))),
   options: {
-    name: 'LskGeneral',
+    name: 'Lsk.js',
   },
   knobs: false,
 };
--- packages/docs/.storybook/webpack.config.js
+++ packages/docs/.storybook/webpack.config.js
@@ -1,4 +1,6 @@
+const webpack = require('webpack');
 const babelrc = require('../.babelrc.js');
+
 module.exports = async ({ config, mode }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
@@ -10,6 +12,27 @@ module.exports = async ({ config, mode }) => {
     babelrc.plugins.splice(transformRuntimeIndex, 1);
   }
   config.module.rules[0].use[0].options.plugins = babelrc.plugins;
+  // config.plugins.push(new webpack.NormalModuleReplacementPlugin(
+  //   /\.\/countriesCodes\.json/,
+  //   require.resolve('./countriesCodes.json', {paths: [ __dirname ]}),
+  // ));
+  config.plugins.push(new webpack.NormalModuleReplacementPlugin(
+    /@lskjs\/dev\/Story/,
+    (resource) => {
+     if (resource.context.endsWith('@lskjs/dev')) {
+       resource.request = require.resolve('@lskjs/dev');
+     }
+   },
+ ));
+  config.plugins.push(new webpack.NormalModuleReplacementPlugin(
+    /createFormWithI18/,
+    (resource) => {
+      resource.request = require.resolve('../src/noop.jsx');
+    },
+  ));
+  config.plugins.push(new webpack.DefinePlugin({
+    __DEV__: 1,
+  }));
   // config.module.rules[0].use[0].options.plugins.push('emotion');
   return config;
 };
