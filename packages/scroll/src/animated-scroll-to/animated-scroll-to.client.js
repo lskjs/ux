615fcbaebe462fdf461a6ed668b980311d758d79
Igor Suvorov <hi@isuvorov.com>
2020-09-01 12:22:32 +0300

    feat(scroll): add option for disabling horisontal scroll

--- packages/scroll/src/animated-scroll-to/animated-scroll-to.client.js
+++ packages/scroll/src/animated-scroll-to/animated-scroll-to.client.js
@@ -1,3 +1,32 @@
 import AnimatedScrollTo from 'animated-scroll-to';
 
-export default AnimatedScrollTo;
+function isElement(el) {
+  return el && el.nodeName;
+}
+function getElementOffset(el) {
+  let top = 0;
+  let left = 0;
+  let element = el;
+
+  // Loop through the DOM tree
+  // and add it's parent's offset to get page offset
+  do {
+    top += element.offsetTop || 0;
+    left += element.offsetLeft || 0;
+    element = element.offsetParent;
+  } while (element);
+
+  return {
+    top,
+    left,
+  };
+}
+
+export default (el, { ignoreHorizontalScroll, ...options } = {}) => {
+  if (ignoreHorizontalScroll && isElement(el)) {
+    const { top } = getElementOffset(el);
+    // eslint-disable-next-line no-param-reassign
+    el = top;
+  }
+  return AnimatedScrollTo(el, options);
+};
