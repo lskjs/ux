02f2573c40a5528f971b6e8709185821ba5bc859
Igor Suvorov <hi@isuvorov.com>
2020-09-15 20:25:52 +0300

    feat(scroll): remove unused packages

--- packages/scroll/src/scrollTo.story.jsx
+++ packages/scroll/src/scrollTo.story.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import range from 'lodash/range';
 import Story from '@lskjs/dev/Story';
 import scrollTo, { setDefaultOptions } from '.';
 
@@ -7,22 +6,13 @@ setDefaultOptions({
   debug: 1,
 });
 
-export default ({ storiesOf }) => (
-  storiesOf('scroll/utils', module)
-    .add('scrollTo', () => (
-      <Story>
-        <button onClick={() => scrollTo(9999)}>
-          bottom
-        </button>
-        {range(100).map(i => (
-          <div>
-            {i}
-          </div>
-        ))}
-        <button onClick={() => scrollTo(0)}>
-          top
-        </button>
-      </Story>
-    ))
-
-);
+export default ({ storiesOf }) =>
+  storiesOf('scroll/utils', module).add('scrollTo', () => (
+    <Story>
+      <button onClick={() => scrollTo(9999)}>bottom</button>
+      {[...Array(100).keys()].map((i) => (
+        <div>{i}</div>
+      ))}
+      <button onClick={() => scrollTo(0)}>top</button>
+    </Story>
+  ));
