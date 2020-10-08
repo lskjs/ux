01180632c02e039693a30b84afcd21aba31b7965
Igor Suvorov <hi@isuvorov.com>
2020-10-08 11:02:06 +0300

    feat(ui2): add chipitem

---
+++ packages/ui2/src/ChipItem/ChipItem.styles.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const ImageWrapper = styled('div')`
+  @media screen and (max-width: 992px) {
+    display: none;
+  }
+`;
+export const TextWrapper = styled('div')``;
