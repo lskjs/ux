7abfd6f98b1fa2e5b313ae3761f7d4224d5ff7bc
Admin <admin@iMac-VikaNaz.local>
2020-10-26 18:31:12 +0400

    fix(Badge): исправили табуляцию

--- packages/ui/src/Badge/Badge.jsx
+++ packages/ui/src/Badge/Badge.jsx
@@ -2,28 +2,27 @@ import React from 'react';
 import { BadgeStyled, Wrapper } from './Badge.styles';
 
 export default class Badge extends React.Component {
-    render () {
-        const { color, backgroundColor, dontShowZero, standalone, position, count } = this.props;
-        const badgeComponent = (
-            <BadgeStyled 
-                color={color} 
-                backgroundColor={backgroundColor} 
-                dontShowZero={dontShowZero} 
-                standalone={standalone} 
-                position={position}
-            >
-                {count}
-            </BadgeStyled>
-        );
-        if (!standalone) {
-            return (
-                <Wrapper>
-                    {this.props.children}
-                    {badgeComponent}
-                </Wrapper>
-                
-            );
-        }
-        return badgeComponent;
+  render () {
+    const { color, backgroundColor, dontShowZero, standalone, position, count } = this.props;
+    const badgeComponent = (
+      <BadgeStyled 
+        color={color} 
+        backgroundColor={backgroundColor} 
+        dontShowZero={dontShowZero} 
+        standalone={standalone} 
+        position={position}
+      >
+          {count}
+      </BadgeStyled>
+    );
+    if (!standalone) {
+      return (
+        <Wrapper>
+          {this.props.children}
+          {badgeComponent}
+        </Wrapper>   
+      );
     }
+    return badgeComponent;
+  }
 }
