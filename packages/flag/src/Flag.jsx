7b54fc7405869cf932e72751708689dc463469ec
Ivan Matskevich <faxtalk666@gmail.com>
2020-10-30 15:33:16 +0300

    feat(flag): change flag package
    Replace 'react-world-flags' with 'react-flags'

--- packages/flag/src/Flag.jsx
+++ packages/flag/src/Flag.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactWorldFlag from 'react-world-flags';
+import ReactFlag from 'react-flags';
 
-const Flag = ({ country, code, height, ...props }) => {
+const Flag = ({ country, code, pngSize, format, height, ...props }) => {
   let countryOrCode = code || country || 'gb';
   if (countryOrCode === 'uk' || countryOrCode === 'en') {
     countryOrCode = 'gb';
   }
-  return <ReactWorldFlag code={countryOrCode.toUpperCase()} height={height} {...props} />;
+  return (
+    <ReactFlag
+      name={countryOrCode.toUpperCase()}
+      format={format}
+      pngSize={pngSize}
+      height={height}
+      basePath="assets"
+      {...props}
+    />
+  );
 };
 
 Flag.propTypes = {
   country: PropTypes.string,
   height: PropTypes.number,
+  format: PropTypes.oneOf(['png', 'icns', 'ico', 'svg']),
+  pngSize: PropTypes.oneOf([16, 24, 32, 48, 64]),
+  code: PropTypes.string,
 };
 Flag.defaultProps = {
   country: 'gb',
+  format: 'svg',
   height: 16,
+  code: null,
+  pngSize: 16,
 };
 
 export default Flag;
