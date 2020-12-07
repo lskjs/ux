271de3f4960eb0200d5ff5e4cff27a252664bf6c
Andrey Kondalov <i@yukio.ru>
2020-12-07 11:19:42 +0400

    fix: disable after release hook

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-lerna publish --skip-npm && \
-if [ "$NODE_ENV" != "production" ]
-then
-  ./scripts/after-release.sh
-else
-  true
-fi
+lerna publish --skip-npm
+# lerna publish --skip-npm && \
+# if [ "$NODE_ENV" != "production" ]
+# then
+#   ./scripts/after-release.sh
+# else
+#   true
+# fi
