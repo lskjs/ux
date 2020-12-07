0f68a2f71cdf2cfc2ce3115baaa618e6c6b0db3b
Andrey Kondalov <i@yukio.ru>
2020-12-07 11:16:29 +0400

    fix(form): remove comments from Select story

--- packages/form/src/controls/Select/Select.story.jsx
+++ packages/form/src/controls/Select/Select.story.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Form, Field } from 'formik';
-// import { ValueContainer as DefaultValueContainer } from 'react-select/lib/components/containers';
 import CheckBlank from 'react-icons2/mdi/checkbox-blank-outline';
 import CheckMarked from 'react-icons2/mdi/checkbox-marked';
 import RadioBlank from 'react-icons2/mdi/checkbox-blank-circle-outline';
@@ -16,7 +15,6 @@ import createForm from '../../createForm';
 import FormDebug from '../../FormDebug';
 import Select from './Select';
 import countriesList from './countriesList';
-// import createFormWithI18 from '../../createFormWithI18';
 
 const SelectFormView = ({ control, ...props }) => (
   <Form>
@@ -78,13 +76,6 @@ const SelectFormView = ({ control, ...props }) => (
   </Form>
 );
 
-// const ValueContainer = ({ children, selectProps, ...props }) => {
-//   const chl = [children[0][0] || children[0], children[1]];
-//   return (
-//     <DefaultValueContainer {...props}>{chl}</DefaultValueContainer>
-//   );
-// };
-
 const SelectForm = createForm({
   view: SelectFormView,
   controls: {
@@ -111,7 +102,6 @@ const SelectForm = createForm({
           title: 'Четыре',
         },
       ],
-      // menuIsOpen: true,
     },
     select: {
       title: 'The Select',
@@ -121,7 +111,6 @@ const SelectForm = createForm({
         value: id,
         title: `The ${id}`,
       })),
-      // menuIsOpen: true,
     },
     select2: {
       title: 'The Select2: without title',
@@ -144,7 +133,6 @@ const SelectForm = createForm({
       title: 'select5 with multisearch',
       component: Select,
       multiSearch: true,
-      // options: range(1, 300),
       options: [
         {
           title: 'Русский',
@@ -225,9 +213,7 @@ const SelectForm = createForm({
         iconActive: <CheckMarked />,
         iconColor: '#1890ff',
       },
-      // hideSelectedOptions: false,
     },
-    // ///
     radio: {
       title: 'Radio',
       component: Select,
@@ -488,235 +474,6 @@ const SelectForm = createForm({
   },
 });
 
-// const SelectFormWithI18 = createFormWithI18(({ i18 }) => ({
-//   view: SelectFormView,
-//   controls: {
-//     select: {
-//       title: 'The Select',
-//       placeholder: 'test',
-//       component: Select,
-//       options: range(1, 11).map(id => ({
-//         value: id,
-//         title: i18.t(`select.${id}`),
-//       })),
-//       // menuIsOpen: true,
-//     },
-//     select2: {
-//       title: 'The Select2: without title',
-//       component: Select,
-//       options: range(1, 11).map(id => ({
-//         value: id,
-//       })),
-//       placeholder: 'placeholder 2',
-//     },
-//     select3: {
-//       title: 'The Select3: options as stings',
-//       component: Select,
-//       options: ['one', 'two'],
-//     },
-//     select4: {
-//       title: 'The Select4',
-//       component: Select,
-//       options: ['one', 'two'],
-//       placeholder: 'placeholder',
-//     },
-//     // ///
-//     multiselect: {
-//       title: 'multiselect: isMulti',
-//       component: Select,
-//       isMulti: true,
-//       options: range(1, 11).map(id => ({
-//         value: id,
-//         id,
-//         title: `User ${id}`,
-//       })),
-//     },
-//     multiselect2: {
-//       title: 'multiselect2: isMulti & hideSelectedOptions',
-//       component: Select,
-//       isMulti: true,
-//       hideSelectedOptions: false,
-//       options: range(1, 11).map(id => ({
-//         value: id,
-//         id,
-//         title: `User ${id}`,
-//       })),
-//     },
-//     multiselect3: {
-//       title: 'multiselect3: isMulti & collapsed',
-//       component: Select,
-//       isMulti: true,
-//       collapsed: true,
-//       options: range(1, 11).map(id => ({
-//         value: id,
-//         id,
-//         title: i18.t(`User ${id}`),
-//       })),
-//     },
-//     checkboxes: {
-//       title: 'checkboxes: isMulti & hideSelectedOptions & collapsed & customView',
-//       component: Select,
-//       isMulti: true,
-//       collapsed: true,
-//       hideSelectedOptions: false,
-//       options: range(1, 11).map(id => ({
-//         value: id,
-//         id,
-//         title: i18.t(`User ${id}`),
-//       })),
-//       optionProps: {
-//         icon: <CheckBlank />,
-//         iconActive: <CheckMarked />,
-//         iconColor: '#1890ff',
-//       },
-//       // hideSelectedOptions: false,
-//     },
-//     // ///
-//     radio: {
-//       title: 'Radio',
-//       component: Select,
-//       options: range(1, 11).map(id => ({
-//         value: id,
-//         id,
-//         title: `User ${id}`,
-//       })),
-//       optionProps: {
-//         icon: <RadioBlank />,
-//         iconActive: <RadioSelected />,
-//         iconColor: '#1890ff',
-//       },
-//     },
-//     userSelect: {
-//       title: 'The userSelect',
-//       component: Select,
-//       options: range(1, 11).map(id => ({
-//         value: id,
-//         id,
-//         image: `https://picsum.photos/40/40/?image=${id}`,
-//         title: `User ${id}`,
-//       })),
-//     },
-//     games: {
-//       title: 'Games',
-//       component: Select,
-//       isMulti: true,
-//       hideSelectedOptions: false,
-//       collapsed: true,
-//       options: range(1, 11).map(id => ({
-//         value: id,
-//         id,
-//         image: `https://picsum.photos/40/40/?image=1${id}`,
-//         title: `Game ${id}`,
-//       })),
-//       optionProps: {
-//         icon: <CheckBlank />,
-//         iconActive: <CheckMarked />,
-//         iconColor: '#1890ff',
-//       },
-//     },
-//     games2: {
-//       title: 'Games2: hideSelectedOptions',
-//       component: Select,
-//       isMulti: true,
-//       hideSelectedOptions: false,
-//       options: range(1, 11).map(id => ({
-//         value: id,
-//         id,
-//         image: `https://picsum.photos/40/40/?image=1${id}`,
-//         title: `Game ${id}`,
-//       })),
-//       optionProps: {
-//         icon: <CheckBlank />,
-//         iconActive: <CheckMarked />,
-//         iconColor: '#1890ff',
-//       },
-//     },
-//     flag: {
-//       title: 'Flag',
-//       component: Select,
-//       options: [
-//         {
-//           title: 'Russia',
-//           value: 'one',
-//           icon: <Flag country="ru" />,
-//         },
-//         {
-//           title: 'Britain',
-//           value: 'two',
-//           icon: <Flag country="gb" />,
-//         },
-//       ],
-//     },
-//     // /////
-//     asyncSelect: {
-//       title: 'The asyncSelect',
-//       component: Select,
-//       async: true,
-//       loadOption: async value => ({
-//         value,
-//         id: value,
-//         image: `https://picsum.photos/40/40/?image=${value}`,
-//         title: `User ${value}`,
-//       }),
-//       loadOptions: async (searchValue = '') => {
-//         const start = searchValue.length;
-//         return new Promise((resolve) => {
-//           setTimeout(() => resolve(range(start, start + 10).map(value => ({
-//             value,
-//             id: value,
-//             image: `https://picsum.photos/40/40/?image=${value}`,
-//             title: `User ${value}`,
-//           }))), 50);
-//         });
-//       },
-//       // return range(start, start + 10).map(value => ({
-//       //   value,
-//       //   id: value,
-//       //   image: `https://picsum.photos/40/40/?image=${value}`,
-//       //   title: `User ${value}`,
-//       // }));
-//       // },
-//     },
-//     countries: {
-//       title: 'Countries',
-//       component: Select,
-//       options: keys(countriesList).map((value) => {
-//         return (
-//           {
-//             title: value,
-//             // title: i18.t(`countries.${value}`),
-//             value,
-//             // icon: () => '@', // <Flag country={value} />,
-//             Icon: () => <Flag country={value} />,
-//           }
-//         );
-//       }),
-//     },
-//     asyncSelect2: {
-//       title: 'The asyncSelect2',
-//       component: Select,
-//       async: true,
-//       loadOption: async value => ({
-//         value,
-//         id: value,
-//         image: `https://picsum.photos/40/40/?image=${value}`,
-//         title: `User ${value}`,
-//       }),
-//       loadOptions: async (searchValue = '') => {
-//         const start = searchValue.length;
-//         return new Promise((resolve) => {
-//           setTimeout(() => resolve(range(start, start + 10).map(value => ({
-//             value,
-//             id: value,
-//             image: `https://picsum.photos/40/40/?image=${value}`,
-//             title: `User ${value}`,
-//           }))), 2000);
-//         });
-//       },
-//     },
-//   },
-// }));
-
 export default ({ storiesOf }) =>
   storiesOf('form/controls', module)
     .add('Select ', () => {
@@ -733,19 +490,6 @@ export default ({ storiesOf }) =>
         </Story>
       );
     })
-    // .add('Select with i18', () => {
-    //   return (
-    //     <Story devtools perf>
-    //       <SelectFormWithI18
-    //         initialValues={{
-    //           select4: 'two',
-    //           asyncSelect2: 99,
-    //           // info: {},
-    //         }}
-    //       />
-    //     </Story>
-    //   );
-    // })
     .add('Modal select', () => {
       return (
         <Story devtools perf>
