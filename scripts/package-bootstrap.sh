38c0b64c5d21c0d7d0b3d25d1bcdf3555ac96af7
Andrey Kondalov <i@yukio.ru>
2021-02-05 16:56:54 +0400

    fix(scripts): ncu bootstrap cli

--- scripts/package-bootstrap.sh
+++ scripts/package-bootstrap.sh
@@ -13,7 +13,7 @@ then
 fi && \
 if [ "$NODE_ENV" != "production" ]
 then
-  ../../node_modules/npm-check-updates/bin/ncu --dep=prod,dev,peer,optional && \
+  ../../node_modules/npm-check-updates/bin/cli.js --dep=prod,dev,peer,optional && \
   npm run bootstrap:nodemodules
 else
   true
