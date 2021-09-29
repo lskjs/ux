93066b56c90badf05da73e76ae5a64d4e7d0c51f
Andrey Kondalov <andrey@kondalov.com>
2021-09-29 12:48:01 +0400

    fix(avatar): new url no-avatar

--- packages/avatar/src/Avatar.jsx
+++ packages/avatar/src/Avatar.jsx
@@ -153,7 +153,7 @@ Avatar.Badge = props => {
 };
 
 Avatar.defaultColors = ['#F8B195', '#F67280', '#C06C84', '#6C5B7B', '#355C7D'];
-Avatar.defaultAvatar = '//cdn.mgbeta.ru/lsk/no-avatar.png';
+Avatar.defaultAvatar = '//lskjs.github.io/assets/images/no-avatar.png';
 Avatar.defaultProps = {
   title: '',
   name: '',
